18494c15ccc26d2099453dbeae8f3afdcb22096c
Jan Verner <jan.verner2@gmail.com>
2019-10-19 02:59:10 +0200

    Add partial solution for Colonel Roche

--- src/main/kotlin/cz/vernjan/ctf/catch19/Roche.kt
+++ src/main/kotlin/cz/vernjan/ctf/catch19/Roche.kt
@@ -1,56 +1,61 @@
 package cz.vernjan.ctf.catch19
 
+import cz.vernjan.ctf.hexToAscii
 import cz.vernjan.ctf.hexToByteArray
+import okhttp3.Headers
 import java.lang.StringBuilder
+import java.net.CookieManager
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpRequest.*
+import java.net.http.HttpResponse
+import java.net.http.HttpResponse.*
 import kotlin.experimental.xor
+import kotlin.system.exitProcess
+
+private val ciphertext =
+    "463216327617246f67406f1266075ec622606c6671765537066636596e621e64e622c2b006066961c66e621f067676e77c6e665167a462c4b50477433617754222d7043542885747df6dd575970417d435223000"
+private val keys = listOf(
+    "5%3B4%3B1%3B3%3B2%3B6",
+    "6%3B5%3B3%3B4%3B1%3B2%3B7",
+    "7%3B6%3B2%3B4%3B5%3B1%3B3%3B8",
+    "2%3B7%3B6%3B5%3B1%3B3%3B4%3B8",
+    "5%3B4%3B3%3B1%3B2%3B6"
+)
 
 fun main() {
 
-    val cipherText =
-        "463216327617246f67406f1266075ec622606c6671765537066636596e621e64e622c2b006066961c66e621f067676e77c6e665167a462c4b50477433617754222d7043542885747df6dd575970417d435223000"
-//    val keys = listOf("roche", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday")
-    val keys = 'a'..'z'
+    val httpClient = HttpClient.newBuilder().build()
 
-    for (len in 4..16) {
-        println()
-        println("Length: $len")
-        keys.forEach { key ->
-            println()
-            println("Key: $key")
-            var noped = false
-            val out = StringBuilder()
-            for ((i, cipherTextByte) in cipherText.hexToByteArray().withIndex()) {
-                if (i % len == 0) {
-                    val decrypted = cipherTextByte.xor(key.toByte())
-                    out.append(decrypted.toChar())
-                    print(decrypted.toChar())
-                    if (decrypted !in 32..127 && decrypted !=10.toByte()) {
-                        noped = true
+    for (key1 in keys) {
+        for (key2 in keys) {
+            println("Keys: $key1, $key2")
 
-//                        print(" Nope!")
-                        break
-                    }
+            // 1 c l
+            // 2 l c
+            // 3 c c
+            val body =
+                "tool=double-transposition-cipher&ciphertext=$ciphertext&permutation1=$key1&permute1=l&permutation2=$key2&permute2=l"
 
-//                    print(decrypted.toChar())
+            val request = newBuilder()
+                .POST(BodyPublishers.ofString(body))
+                .uri(URI.create("https://www.dcode.fr/api/"))
+                .header("content-type", "application/x-www-form-urlencoded; charset=UTF-8")
+                .build()
+
+            val response = httpClient.send(request, BodyHandlers.ofString())
+            if (response.statusCode() == 200) {
+                val result = response.body()!!.split('"')[3]
+                println(response)
+                println(result)
+                println(result.hexToAscii())
+                if (result.hexToAscii().contains("FLAG")) {
+                    println("BINGO!")
+                    exitProcess(0)
                 }
-
-//                val keyShift = key[i % key.length].toByte() - 'a'.toByte()
-
-//            val decrypted = cipherTextByte - keyShift
-//            println("$cipherTextByte + $keyShift = $decrypted (${decrypted.toChar()})")
-
-//            println("$i: $keyByte: $byte")
-
             }
-            if (!noped || out.length >= 7) {
-
-                println("YES: $out")
-            }
-
+//            println(response.hexToAscii())
         }
-//        println()
-//        println("***")
     }
-
-
 }
