22b30a10a1d8a513831ff15fea7f8d0c95e1bf56
Jan Verner <jan.verner2@gmail.com>
2019-12-20 08:56:34 +0100

    Add solution for Day 20

---
+++ src/main/kotlin/cz/vernjan/ctf/hv19/day20/ReadFlag.kt
@@ -0,0 +1,46 @@
+package cz.vernjan.ctf.hv19.day20
+
+import java.nio.file.Files
+import java.nio.file.Paths
+import kotlin.experimental.xor
+
+fun main() {
+    val acc = mutableListOf(0xce.toByte(), 0x55.toByte(), 0x95.toByte(), 0x4e.toByte(), 0x38.toByte(), 0xc5.toByte(), 0x89.toByte(), 0xa5.toByte(), 0x1b.toByte(), 0x6f.toByte(), 0x5e.toByte(), 0x25.toByte(), 0xd2.toByte(), 0x1d.toByte(), 0x2a.toByte(), 0x2b.toByte(), 0x5e.toByte(), 0x7b.toByte(), 0x39.toByte(), 0x14.toByte(), 0x8e.toByte(), 0xd0.toByte(), 0xf0.toByte(), 0xf8.toByte(), 0xf8.toByte(), 0xa5.toByte())
+    val fileData = Files.readAllBytes(Paths.get("d:\\Shared\\505Retail.PUP"))
+
+    for (i in 0x1337 until 0x1714908 step 0x1337) {
+        val chunk = fileData.copyOfRange(i, i + 26)
+        chunk.forEachIndexed{j, b ->
+           acc[j] = (b xor acc[j])
+        }
+    }
+
+    println(String(acc.toByteArray()))
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- write-ups/Hackvent2019/README.md
+++ write-ups/Hackvent2019/README.md
@@ -20,6 +20,7 @@ Solutions for CTF [Hackvent 2019](https://academy.hacking-lab.com/events/6)
 - [HV19.16 B0rked Calculator (hard)](day16/README.md)
 - [HV19.17 Unicode Portal (hard)](day17/README.md)
 - [HV19.18 Dance with me (hard)](day18/README.md)
+- [HV19.19 🎅 (hard)](day19/README.md)
 
 
 ## Hiddens
