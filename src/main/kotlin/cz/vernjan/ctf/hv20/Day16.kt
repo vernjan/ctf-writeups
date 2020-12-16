68581eb6c97a0c18b40f1b049f577d2ee17d7871
Jan Verner <jan.verner@citrix.com>
2020-12-16 21:40:41 +0100

    Add HV20 day 16

--- src/main/kotlin/cz/vernjan/ctf/hv20/Day16.kt
+++ src/main/kotlin/cz/vernjan/ctf/hv20/Day16.kt
@@ -2,116 +2,99 @@ package cz.vernjan.ctf.hv20
 
 import java.lang.StringBuilder
 
-var grid = arrayOf(
-    charArrayOf('#', '#', '#', '6', '_', 'e', '#', '#', '#', '#', '#', '#'),
-    charArrayOf('#', '#', '#', 'i', '{', 'a', '#', '#', '#', '#', '#', '#'),
-    charArrayOf('#', '#', '#', 'e', 's', '3', '#', '#', '#', '#', '#', '#'),
-    charArrayOf('H', 'V', '7', '_', 'w', 'e', 'o', '@', 's', 'i', 's', 'l'),
-    charArrayOf('h', '_', 'e', '0', 'k', '_', '_', 't', '_', 'n', 's', 'o'),
-    charArrayOf('o', 'a', '_', 'c', 'd', 'a', '4', 'r', '5', '2', 'c', '_'),
-    charArrayOf('#', '#', '#', '_', 'n', 's', '#', '#', '#', '#', '#', '#'),
-    charArrayOf('#', '#', '#', 'l', 'l', 't', '#', '#', '#', '#', '#', '#'),
-    charArrayOf('#', '#', '#', '}', 'p', 'h', '#', '#', '#', '#', '#', '#')
+var cube = arrayOf(
+        charArrayOf('#', '#', '#', '6', '_', 'e', '#', '#', '#', '#', '#', '#'),
+        charArrayOf('#', '#', '#', 'i', '{', 'a', '#', '#', '#', '#', '#', '#'),
+        charArrayOf('#', '#', '#', 'e', 's', '3', '#', '#', '#', '#', '#', '#'),
+        charArrayOf('H', 'V', '7', '_', 'w', 'e', 'o', '@', 's', 'i', 's', 'l'),
+        charArrayOf('h', '_', 'e', '0', 'k', '_', '_', 't', '_', 'n', 's', 'o'),
+        charArrayOf('o', 'a', '_', 'c', 'd', 'a', '4', 'r', '5', '2', 'c', '_'),
+        charArrayOf('#', '#', '#', '_', 'n', 's', '#', '#', '#', '#', '#', '#'),
+        charArrayOf('#', '#', '#', 'l', 'l', 't', '#', '#', '#', '#', '#', '#'),
+        charArrayOf('#', '#', '#', '}', 'p', 'h', '#', '#', '#', '#', '#', '#')
 )
 
-val gridClone = grid.map { it.clone() }.toTypedArray()
+private fun cloneCube(cube: Array<CharArray>) = cube.map { it.clone() }.toTypedArray()
 
-fun LR(col1: Int, col2: Int) = linkedMapOf(
-    Pair(0, col1) to Pair(3, col2),
-    Pair(3, col2) to Pair(4, col2),
-    Pair(4, col2) to Pair(5, col2),
-    Pair(5, col2) to Pair(8, col1),
-    Pair(8, col1) to Pair(7, col1),
-    Pair(7, col1) to Pair(6, col1),
-    Pair(6, col1) to Pair(5, col1),
-    Pair(5, col1) to Pair(4, col1),
-    Pair(4, col1) to Pair(3, col1),
-    Pair(3, col1) to Pair(2, col1),
-    Pair(2, col1) to Pair(1, col1)
+val cubeOrig = cloneCube(cube)
+
+fun leftRight(col1: Int, col2: Int) = linkedMapOf(
+        Pair(0, col1) to Pair(3, col2),
+        Pair(3, col2) to Pair(4, col2),
+        Pair(4, col2) to Pair(5, col2),
+        Pair(5, col2) to Pair(8, col1),
+        Pair(8, col1) to Pair(7, col1),
+        Pair(7, col1) to Pair(6, col1),
+        Pair(6, col1) to Pair(5, col1),
+        Pair(5, col1) to Pair(4, col1),
+        Pair(4, col1) to Pair(3, col1),
+        Pair(3, col1) to Pair(2, col1),
+        Pair(2, col1) to Pair(1, col1)
 )
 
-fun UD(row: Int) = linkedMapOf(
-    Pair(row, 11) to Pair(row, 0),
-    Pair(row, 0) to Pair(row, 1),
-    Pair(row, 1) to Pair(row, 2),
-    Pair(row, 2) to Pair(row, 3),
-    Pair(row, 3) to Pair(row, 4),
-    Pair(row, 4) to Pair(row, 5),
-    Pair(row, 5) to Pair(row, 6),
-    Pair(row, 6) to Pair(row, 7),
-    Pair(row, 7) to Pair(row, 8),
-    Pair(row, 8) to Pair(row, 9),
-    Pair(row, 9) to Pair(row, 10)
+fun upperDown(row: Int) = linkedMapOf(
+        Pair(row, 11) to Pair(row, 0),
+        Pair(row, 0) to Pair(row, 1),
+        Pair(row, 1) to Pair(row, 2),
+        Pair(row, 2) to Pair(row, 3),
+        Pair(row, 3) to Pair(row, 4),
+        Pair(row, 4) to Pair(row, 5),
+        Pair(row, 5) to Pair(row, 6),
+        Pair(row, 6) to Pair(row, 7),
+        Pair(row, 7) to Pair(row, 8),
+        Pair(row, 8) to Pair(row, 9),
+        Pair(row, 9) to Pair(row, 10)
 )
 
-fun F() = linkedMapOf(
-    Pair(2, 3) to Pair(3, 2),
-    Pair(3, 2) to Pair(4, 2),
-    Pair(4, 2) to Pair(5, 2),
-    Pair(5, 2) to Pair(6, 3),
-    Pair(6, 3) to Pair(6, 4),
-    Pair(6, 4) to Pair(6, 5),
-    Pair(6, 5) to Pair(5, 6),
-    Pair(5, 6) to Pair(4, 6),
-    Pair(4, 6) to Pair(3, 6),
-    Pair(3, 6) to Pair(2, 5),
-    Pair(2, 5) to Pair(2, 4)
+fun front() = linkedMapOf(
+        Pair(2, 3) to Pair(3, 2),
+        Pair(3, 2) to Pair(4, 2),
+        Pair(4, 2) to Pair(5, 2),
+        Pair(5, 2) to Pair(6, 3),
+        Pair(6, 3) to Pair(6, 4),
+        Pair(6, 4) to Pair(6, 5),
+        Pair(6, 5) to Pair(5, 6),
+        Pair(5, 6) to Pair(4, 6),
+        Pair(4, 6) to Pair(3, 6),
+        Pair(3, 6) to Pair(2, 5),
+        Pair(2, 5) to Pair(2, 4)
 )
 
-fun B() = linkedMapOf(
-    Pair(0, 3) to Pair(0, 4),
-    Pair(0, 4) to Pair(0, 5),
-    Pair(0, 5) to Pair(3, 8),
-    Pair(3, 8) to Pair(4, 8),
-    Pair(4, 8) to Pair(5, 8),
-    Pair(5, 8) to Pair(8, 5),
-    Pair(8, 5) to Pair(8, 4),
-    Pair(8, 4) to Pair(8, 3),
-    Pair(8, 3) to Pair(5, 0),
-    Pair(5, 0) to Pair(4, 0),
-    Pair(4, 0) to Pair(3, 0)
+fun back() = linkedMapOf(
+        Pair(0, 3) to Pair(0, 4),
+        Pair(0, 4) to Pair(0, 5),
+        Pair(0, 5) to Pair(3, 8),
+        Pair(3, 8) to Pair(4, 8),
+        Pair(4, 8) to Pair(5, 8),
+        Pair(5, 8) to Pair(8, 5),
+        Pair(8, 5) to Pair(8, 4),
+        Pair(8, 4) to Pair(8, 3),
+        Pair(8, 3) to Pair(5, 0),
+        Pair(5, 0) to Pair(4, 0),
+        Pair(4, 0) to Pair(3, 0)
 )
 
 fun main() {
-//    printGrid()
-//    MoveCube.R.move()
-//    printGrid()
-
-    // System.exit(0)
-
-//    for (moveCube in MoveCube.values()) {
-//        grid = gridClone.map { it.clone() }.toTypedArray()
-//        println("Move cube: $moveCube")
-
     for (move1 in Move.values()) {
-        println("Move: $move1")
+        println("First move: $move1")
         for (move2 in Move.values()) {
             for (move3 in Move.values()) {
                 for (move4 in Move.values()) {
                     for (move5 in Move.values()) {
-                        grid = gridClone.map { it.clone() }.toTypedArray()
-                        move1.move()
-                        move2.move()
-                        move3.move()
-                        move4.move()
-                        move5.move()
-                        val result = gridToString()
-                        if (result.contains("HV20{")) {
+                        cube = cloneCube(cubeOrig)
+                        move1.doIt()
+                        move2.doIt()
+                        move3.doIt()
+                        move4.doIt()
+                        move5.doIt()
+                        val result = readCube()
+                        if (result.matches("^HV20\\{.*}$".toRegex())) {
                             println(result)
-                            //printGrid()
+                            println("$move1 -> $move2 -> $move3 -> $move4 -> $move5")
+                            printCubeIn2D()
                         }
-//                        for (move6 in Move.values()) {
-//                            move6.move()
-//                            val result = gridToString()
-//                            if (result.contains("HV20{")) {
-//                                println(result)
-//                            }
-//                        }
-
                     }
-//                    }
                 }
-
             }
         }
     }
@@ -124,258 +107,180 @@ fun main() {
 enum class Move {
 
     LEFT {
-        override fun move() {
+        override fun doIt() {
             rotFaceRight(3, 0)
-            rotSide(LR(3, 11))
+            rotSide(leftRight(3, 11))
         }
     },
     LEFT2 {
-        override fun move() {
-            LEFT.move()
-            LEFT.move()
+        override fun doIt() {
+            LEFT.doIt()
+            LEFT.doIt()
         }
     },
     LEFT_R {
-        override fun move() {
+        override fun doIt() {
             rotFaceLeft(3, 0)
-            rotSide(reverse(LR(3, 11)))
+            rotSide(reverse(leftRight(3, 11)))
         }
     },
     RIGHT {
-        override fun move() {
+        override fun doIt() {
             rotFaceRight(3, 6)
-            rotSide(reverse(LR(5, 9)))
+            rotSide(reverse(leftRight(5, 9)))
         }
     },
     RIGHT2 {
-        override fun move() {
-            RIGHT.move()
-            RIGHT.move()
+        override fun doIt() {
+            RIGHT.doIt()
+            RIGHT.doIt()
         }
     },
     RIGHT_R {
-        override fun move() {
+        override fun doIt() {
             rotFaceLeft(3, 6)
-            rotSide(LR(5, 9))
+            rotSide(leftRight(5, 9))
         }
     },
     UP {
-        override fun move() {
+        override fun doIt() {
             rotFaceRight(0, 3)
-            rotSide(UD(3))
+            rotSide(upperDown(3))
         }
     },
     UP2 {
-        override fun move() {
-            UP.move()
-            UP.move()
+        override fun doIt() {
+            UP.doIt()
+            UP.doIt()
         }
     },
     UP_R {
-        override fun move() {
+        override fun doIt() {
             rotFaceLeft(0, 3)
-            rotSide(reverse(UD(3)))
+            rotSide(reverse(upperDown(3)))
         }
     },
     DOWN {
-        override fun move() {
+        override fun doIt() {
             rotFaceRight(6, 3)
-            rotSide(reverse(UD(5)))
+            rotSide(reverse(upperDown(5)))
         }
     },
     DOWN2 {
-        override fun move() {
-            DOWN.move()
-            DOWN.move()
+        override fun doIt() {
+            DOWN.doIt()
+            DOWN.doIt()
         }
     },
     DOWN_R {
-        override fun move() {
+        override fun doIt() {
             rotFaceLeft(6, 3)
-            rotSide(UD(5))
+            rotSide(upperDown(5))
         }
     },
     FRONT {
-        override fun move() {
+        override fun doIt() {
             rotFaceRight(3, 3)
-            rotSide(F())
+            rotSide(front())
         }
     },
     FRONT2 {
-        override fun move() {
-            FRONT.move()
-            FRONT.move()
+        override fun doIt() {
+            FRONT.doIt()
+            FRONT.doIt()
         }
     },
     FRONT_R {
-        override fun move() {
+        override fun doIt() {
             rotFaceLeft(3, 3)
-            rotSide(reverse(F()))
+            rotSide(reverse(front()))
         }
     },
     BACK {
-        override fun move() {
+        override fun doIt() {
             rotFaceRight(3, 9)
-            rotSide(B())
+            rotSide(back())
         }
     },
     BACK2 {
-        override fun move() {
-            BACK.move()
-            BACK.move()
+        override fun doIt() {
+            BACK.doIt()
+            BACK.doIt()
         }
     },
     BACK_R {
-        override fun move() {
+        override fun doIt() {
             rotFaceLeft(3, 9)
-            rotSide(reverse(B()))
+            rotSide(reverse(back()))
         }
     };
 
-    abstract fun move()
+    abstract fun doIt()
 }
 
-enum class MoveSpecial {
+private fun reverse(steps: Map<Pair<Int, Int>, Pair<Int, Int>>) =
+        steps.entries.reversed().map { entry -> entry.value to entry.key }.toMap()
 
-    MIDDLE_LR_BOTTOM {
-        override fun move() {
-            rotSide(LR(4, 10))
-        }
-    },
-
-    MIDDLE_UD_LEFT {
-        override fun move() {
-            rotSide(UD(4))
-        }
-    };
-
-    // TODO MIDDLE_FB_RIGHT
-
-    abstract fun move()
-
-}
-
-enum class MoveCube {
-    F {
-        override fun move() {
-            // no-op
-        }
-    },
-    R {
-        override fun move() {
-            Move.UP.move()
-            MoveSpecial.MIDDLE_UD_LEFT.move()
-            Move.DOWN_R.move()
-        }
-    },
-    B {
-        override fun move() {
-            R.move()
-            R.move()
-        }
-    },
-    L {
-        override fun move() {
-            R.move()
-            R.move()
-            R.move()
-        }
-    };
-//    T, P;
-
-    abstract fun move()
-}
-
-
-/**
- * Helper methods:
- */
-
-private fun reverse(shifts: Map<Pair<Int, Int>, Pair<Int, Int>>) =
-    shifts.entries.reversed().map { entry -> entry.value to entry.key }.toMap()
-
-private fun rotSide(shifts: Map<Pair<Int, Int>, Pair<Int, Int>>) {
+private fun rotSide(steps: Map<Pair<Int, Int>, Pair<Int, Int>>) {
     repeat(3) {
-        val temp = grid[shifts.keys.first().first][shifts.keys.first().second]
-        shifts.forEach { (to, from) ->
-            grid[to.first][to.second] = grid[from.first][from.second]
+        val temp = cube[steps.keys.first().first][steps.keys.first().second]
+        steps.forEach { (to, from) ->
+            cube[to.first][to.second] = cube[from.first][from.second]
         }
-        grid[shifts.values.last().first][shifts.values.last().second] = temp
+        cube[steps.values.last().first][steps.values.last().second] = temp
     }
 }
 
 private fun rotFaceLeft(upperIndex: Int, leftIndex: Int) {
-    var temp = grid[upperIndex][leftIndex]
-    grid[upperIndex][leftIndex] = grid[upperIndex][leftIndex + 2]
-    grid[upperIndex][leftIndex + 2] = grid[upperIndex + 2][leftIndex + 2]
-    grid[upperIndex + 2][leftIndex + 2] = grid[upperIndex + 2][leftIndex]
-    grid[upperIndex + 2][leftIndex] = temp
+    var temp = cube[upperIndex][leftIndex]
+    cube[upperIndex][leftIndex] = cube[upperIndex][leftIndex + 2]
+    cube[upperIndex][leftIndex + 2] = cube[upperIndex + 2][leftIndex + 2]
+    cube[upperIndex + 2][leftIndex + 2] = cube[upperIndex + 2][leftIndex]
+    cube[upperIndex + 2][leftIndex] = temp
 
-    temp = grid[upperIndex][leftIndex + 1]
-    grid[upperIndex][leftIndex + 1] = grid[upperIndex + 1][leftIndex + 2]
-    grid[upperIndex + 1][leftIndex + 2] = grid[upperIndex + 2][leftIndex + 1]
-    grid[upperIndex + 2][leftIndex + 1] = grid[upperIndex + 1][leftIndex]
-    grid[upperIndex + 1][leftIndex] = temp
+    temp = cube[upperIndex][leftIndex + 1]
+    cube[upperIndex][leftIndex + 1] = cube[upperIndex + 1][leftIndex + 2]
+    cube[upperIndex + 1][leftIndex + 2] = cube[upperIndex + 2][leftIndex + 1]
+    cube[upperIndex + 2][leftIndex + 1] = cube[upperIndex + 1][leftIndex]
+    cube[upperIndex + 1][leftIndex] = temp
 }
 
 private fun rotFaceRight(upperIndex: Int, leftIndex: Int) {
-    var temp = grid[upperIndex][leftIndex]
-    grid[upperIndex][leftIndex] = grid[upperIndex + 2][leftIndex]
-    grid[upperIndex + 2][leftIndex] = grid[upperIndex + 2][leftIndex + 2]
-    grid[upperIndex + 2][leftIndex + 2] = grid[upperIndex][leftIndex + 2]
-    grid[upperIndex][leftIndex + 2] = temp
+    var temp = cube[upperIndex][leftIndex]
+    cube[upperIndex][leftIndex] = cube[upperIndex + 2][leftIndex]
+    cube[upperIndex + 2][leftIndex] = cube[upperIndex + 2][leftIndex + 2]
+    cube[upperIndex + 2][leftIndex + 2] = cube[upperIndex][leftIndex + 2]
+    cube[upperIndex][leftIndex + 2] = temp
 
-    temp = grid[upperIndex][leftIndex + 1]
-    grid[upperIndex][leftIndex + 1] = grid[upperIndex + 1][leftIndex]
-    grid[upperIndex + 1][leftIndex] = grid[upperIndex + 2][leftIndex + 1]
-    grid[upperIndex + 2][leftIndex + 1] = grid[upperIndex + 1][leftIndex + 2]
-    grid[upperIndex + 1][leftIndex + 2] = temp
+    temp = cube[upperIndex][leftIndex + 1]
+    cube[upperIndex][leftIndex + 1] = cube[upperIndex + 1][leftIndex]
+    cube[upperIndex + 1][leftIndex] = cube[upperIndex + 2][leftIndex + 1]
+    cube[upperIndex + 2][leftIndex + 1] = cube[upperIndex + 1][leftIndex + 2]
+    cube[upperIndex + 1][leftIndex + 2] = temp
 }
 
-fun gridToString(): String {
+fun readCube(): String {
     val sb = StringBuilder()
-    sb.append(faceToString(3, 3))
-    sb.append(faceToString(3, 6))
-    sb.append(faceToString(3, 9))
-    sb.append(faceToString(3, 0))
-    sb.append(faceToString(0, 3))
-    sb.append(faceToString(6, 3))
+    sb.append(readFace(0, 3))
+    sb.append(readFace(3, 0))
+    sb.append(readFace(3, 3))
+    sb.append(readFace(3, 6))
+    sb.append(readFace(3, 9))
+    sb.append(readFace(6, 3))
     return sb.toString()
 }
 
-fun faceToString(upperIndex: Int, leftIndex: Int): String {
+fun readFace(upperIndex: Int, leftIndex: Int): String {
     val sb = StringBuilder()
     for (i in 0..2) {
         for (j in 0..2) {
-            sb.append(grid[upperIndex + i][leftIndex + j])
+            sb.append(cube[upperIndex + i][leftIndex + j])
         }
     }
     return sb.toString()
 }
 
-fun printGrid() {
-    println(grid.joinToString("\n") { it.toList().chunked(3).joinToString(" ") })
+fun printCubeIn2D() {
+    println(cube.joinToString("\n") { it.toList().chunked(3).joinToString(" ") })
     println("")
-}
-
-/**
- * Tests:
- */
-
-private fun doSomeTests() {
-    Move.RIGHT.move()
-    Move.RIGHT_R.move()
-    verifyItIsTheSame()
-
-    Move.LEFT.move()
-    Move.LEFT_R.move()
-    verifyItIsTheSame()
-}
-
-private fun verifyItIsTheSame() {
-    grid.forEachIndexed { i, arr ->
-        if (!gridClone[i].contentEquals(arr)) {
-            throw AssertionError("FUCK at $i!")
-        }
-    }
 }
--- write-ups/Hackvent2020/day16/README.md
+++ write-ups/Hackvent2020/day16/README.md
@@ -2,3 +2,125 @@
 
 _Santa loves to keep his personal secrets on a little toy cube he got from a kid called Bread. Turns out that was not a very good idea. Last night Rudolph got hold of it and frubl'd it about five times before spitting it out. Look at it! All the colors have come off! Naughty Rudolph!_
 
+[Download](cube.stl)
+
+---
+
+[STL](https://en.wikipedia.org/wiki/STL_(file_format)) is a format for 3D models. I opened the file with Windows native
+[Paint 3D](https://en.wikipedia.org/wiki/Paint_3D). There is, again, a Rubik's cube:
+
+![](cube.png)
+
+I added colors to make the characters readable:
+
+![](cube-colors.png) 
+
+At this point, I thought it's gonna be easy... According the description, Rudolph "frubl'd it about five times".
+I took that literally, and started reversing it as `d'lb'u'r'f'`. I did it in Excel:
+
+![](excel.png)
+
+No flag appeared. Then I thought I should do 5x `d'lb'u'r'f'`. So I recorded Excel marcos to make it fast.
+Even though it worked quite nice, it was not the right solution.
+
+The goal of this challenge was, indeed, to **brute-force all the possible moves**.
+
+Shame I wasted 2 hours doing ^^ but it came handy when I started programming the challenge.
+The full source code is a bit long so you can read it [here](../../../src/main/kotlin/cz/vernjan/ctf/hv20/Day16.kt).
+ 
+This is just a snippet:
+```kotlin
+fun main() {
+    for (move1 in Move.values()) {
+        println("Move: $move1")
+        for (move2 in Move.values()) {
+            for (move3 in Move.values()) {
+                for (move4 in Move.values()) {
+                    for (move5 in Move.values()) {
+                        cube = cloneCube(cubeOrig)
+                        move1.doIt()
+                        move2.doIt()
+                        move3.doIt()
+                        move4.doIt()
+                        move5.doIt()
+                        val result = readCube()
+                        if (result.matches("^HV20\\{.*}$".toRegex())) {
+                            println(result)
+                            println("$move1 -> $move2 -> $move3 -> $move4 -> $move5")
+                            printCubeIn2D()
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+```
+
+It prints:
+```
+First move: LEFT
+First move: LEFT2
+First move: LEFT_R
+HV20{no_sle3p_sihce_4wks_lea_5_tr_n_hi6tscoae_a7olds@}
+LEFT_R -> FRONT_R -> RIGHT -> BACK_R -> RIGHT2
+[#, #, #] [H, V, 2] [#, #, #] [#, #, #]
+[#, #, #] [0, {, n] [#, #, #] [#, #, #]
+[#, #, #] [o, _, s] [#, #, #] [#, #, #]
+[l, e, 3] [e, _, 4] [a, _, 5] [h, i, 6]
+[p, _, s] [w, k, s] [_, t, r] [t, s, c]
+[i, h, c] [_, l, e] [_, n, _] [o, a, e]
+[#, #, #] [_, a, 7] [#, #, #] [#, #, #]
+[#, #, #] [o, l, d] [#, #, #] [#, #, #]
+[#, #, #] [s, @, }] [#, #, #] [#, #, #]
+
+HV20{no_sle3p_since_4wks_lead5_to_@_hi6hscore_a7_last}
+LEFT_R -> BACK_R -> DOWN -> FRONT_R -> RIGHT2
+[#, #, #] [H, V, 2] [#, #, #] [#, #, #]
+[#, #, #] [0, {, n] [#, #, #] [#, #, #]
+[#, #, #] [o, _, s] [#, #, #] [#, #, #]
+[l, e, 3] [e, _, 4] [a, d, 5] [h, i, 6]
+[p, _, s] [w, k, s] [_, t, o] [h, s, c]
+[i, n, c] [_, l, e] [_, @, _] [o, r, e]
+[#, #, #] [_, a, 7] [#, #, #] [#, #, #]
+[#, #, #] [_, l, a] [#, #, #] [#, #, #]
+[#, #, #] [s, t, }] [#, #, #] [#, #, #]
+
+First move: RIGHT
+First move: RIGHT2
+First move: RIGHT_R
+First move: UP
+First move: UP2
+First move: UP_R
+First move: DOWN
+First move: DOWN2
+First move: DOWN_R
+First move: FRONT
+First move: FRONT2
+First move: FRONT_R
+First move: BACK
+HV20{ncsele_s_aida_w7lka4r3__5@too__hi6hs_ocestenl_sp}
+BACK -> LEFT_R -> DOWN_R -> BACK_R -> RIGHT_R
+[#, #, #] [H, V, 2] [#, #, #] [#, #, #]
+[#, #, #] [0, {, n] [#, #, #] [#, #, #]
+[#, #, #] [c, s, e] [#, #, #] [#, #, #]
+[l, e, _] [_, w, 7] [_, _, 5] [h, i, 6]
+[s, _, a] [l, k, a] [@, t, o] [h, s, _]
+[i, d, a] [4, r, 3] [o, _, _] [o, c, e]
+[#, #, #] [s, t, e] [#, #, #] [#, #, #]
+[#, #, #] [n, l, _] [#, #, #] [#, #, #]
+[#, #, #] [s, p, }] [#, #, #] [#, #, #]
+
+First move: BACK2
+First move: BACK_R
+```
+
+There are 3 possible flags (I neglected character rotations in my solution) but obviously,
+this one correct: `HV20{no_sle3p_since_4wks_lead5_to_@_hi6hscore_a7_last}`
+
+---
+
+I might be a bit lucky with choosing the default cube orientation. I took the one I saw in Paint 3D before doing
+any manipulations:
+
+![](start.png)
