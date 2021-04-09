435900997713e54a66f629f81614000f03995c11
Jan Verner <jan.verner@citrix.com>
2021-04-09 16:09:18 +0200

    Add HE2021 challenges

--- src/main/kotlin/cz/vernjan/ctf/he21/LotsOfJWTs.kt
+++ src/main/kotlin/cz/vernjan/ctf/he21/LotsOfJWTs.kt
@@ -2,7 +2,6 @@ package cz.vernjan.ctf.he21
 
 import com.auth0.jwt.JWT
 import cz.vernjan.ctf.Resources
-import cz.vernjan.ctf.decodeBaseUrl64
 
 fun main() {
     val token = Resources.asString("he21/jwts.txt")
@@ -11,19 +10,13 @@ fun main() {
 
 fun printJwt(token: String) {
     val jwt = JWT.decode(token)
-//    println("---")
-//    println(jwt.header.decodeBaseUrl64())
-//    println(jwt.claims.keys)
-//    println(jwt.signature.decodeBaseUrl64())
-
-    jwt.claims.keys.forEach {
-        val claimValue = jwt.getClaim(it).asString()
+    jwt.claims.keys.forEach { key ->
+        val claimValue = jwt.getClaim(key).asString()
         if (claimValue.contains(".")) {
             printJwt(claimValue)
         } else {
-//            println("$it: $claimValue")
-            if (it != "iss" && !it.startsWith("ct")) {
-                println("$it: $claimValue")
+            if (key != "iss" && !key.startsWith("ct")) {
+                println("$key $claimValue")
             }
         }
     }
--- write-ups/HackyEaster2021/level6/lots-of-jwts/README.md
+++ write-ups/HackyEaster2021/level6/lots-of-jwts/README.md
@@ -5,6 +5,36 @@ So many JWTs! What do they hide?
 
 ---
 
+I decoded the token at https://jwt.io/. It contains more tokens. And those tokens contain another tokens...
+
+This is job for a small utility program:
+```kotlin
+import com.auth0.jwt.JWT
+import cz.vernjan.ctf.Resources
+
+fun main() {
+    val token = Resources.asString("he21/jwts.txt")
+    printJwt(token)
+}
+
+fun printJwt(token: String) {
+    val jwt = JWT.decode(token)
+    jwt.claims.keys.forEach { key ->
+        val claimValue = jwt.getClaim(key).asString()
+        if (claimValue.contains(".")) {
+            printJwt(claimValue)
+        } else {
+            if (key != "iss" && !key.startsWith("ct")) {
+                println("$key $claimValue")
+            }
+        }
+    }
+}
+```
+
+It recursively decodes all the tokens and prints the claims which are not `iss` or `ct*`.
+
+The output is:
 ```
 i: he202
 iv: f_js0
@@ -14,6 +44,7 @@ vi: k3nZ}
 ii: 1{pl3
 ```
 
+Rearrange it:
 ```
 i: he202
 ii: 1{pl3
