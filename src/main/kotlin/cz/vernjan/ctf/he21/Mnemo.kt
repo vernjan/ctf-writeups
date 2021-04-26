0f53203b59cee65b19671d445ce40d0037c232b9
Jan Verner <jan.verner@citrix.com>
2021-04-26 16:56:59 +0200

    Add HE2021 challenges

--- src/main/kotlin/cz/vernjan/ctf/he21/Mnemo.kt
+++ src/main/kotlin/cz/vernjan/ctf/he21/Mnemo.kt
@@ -1,41 +1,59 @@
 package cz.vernjan.ctf.he21
 
 import cz.vernjan.ctf.Resources
+import kotlin.system.exitProcess
 
-val list = Resources.asLines("he21/bip39-list.txt")
-val chars = ('0'..'9') + ('a'..'z') + ('A'..'Z') + '_' + '$'
+val wordlist = Resources.asLines("he21/bip39-list.txt")
+
+//val flagChars = ('0'..'9') + ('a'..'z') + ('A'..'Z') + '_' + '$'
+val flagChars = ('0'..'9') + ('a'..'z') + '_' + '!' + '}'
+val words = mutableListOf(
+    "adapt", "bind", "blind", "craft", "garage", "hip", "home", "hotel", "lonely", "magnet",
+    "mushroom", "napkin", "reason", "rescue", "ring", "shift", "small", "sunset", "tongue"
+)
 
 fun main() {
+//    searchFlags(listOf(FlagContext("he2021{", words)))
+    searchFlags(listOf(FlagContext("he2021{", words)))
+    exitProcess(1)
+}
 
-    val phrase = mutableListOf(
-        "adapt", "bind", "blind", "craft", "garage", "hip", "home", "hotel",
-        "lonely", "magnet", "mushroom", "napkin", "reason", "rescue", "ring", "shift", "small", "sunset",
-        "tongue"
-    )
+private fun searchFlags(flagContexts: List<FlagContext>) {
+    for (flagContext in flagContexts) {
+        val flag = flagContext.flag
 
-    val flags = listOf("he2021{", "he2021{f", "he2021{n", "he2021{t")
+//        println("Expanding flag $flag")
+//        Thread.sleep(500)
 
-    flags.forEach { flag ->
-        println("Testing flag: $flag")
-        val extraBits = (flag.length * 8) % 11
-        if (extraBits < 3) {
-            for (ch1 in chars) {
-                for (ch2 in chars) {
-                    doGuess("$flag$ch1$ch2", phrase)
+        val newFlags = mutableListOf<FlagContext>()
+        val unusedBits = (flag.length * 8) % 11
+        if (unusedBits < 3) {
+            for (ch1 in flagChars) {
+                for (ch2 in flagChars) {
+                    val newFlag = testFlag("$flag$ch1$ch2", flagContext.words)
+                    if (newFlag != null) {
+                        newFlags.add(newFlag)
+                    }
                 }
             }
         } else {
-            for (ch1 in chars) {
-                doGuess("$flag$ch1", phrase)
+            for (ch1 in flagChars) {
+                val newFlag = testFlag("$flag$ch1", flagContext.words)
+                if (newFlag != null) {
+                    newFlags.add(newFlag)
+                }
             }
         }
+        searchFlags(newFlags)
     }
 }
 
-private fun doGuess(
-    flag: String,
-    phrase: MutableList<String>
-) {
+private fun testFlag(flag: String, words: List<String>): FlagContext? {
+    if (words.isEmpty()) {
+        println("No more words left! Flag is: $flag")
+        exitProcess(0)
+    }
+
 //    println("Testing flag: $flag")
 
     val indexes = flag.toByteArray()
@@ -43,15 +61,25 @@ private fun doGuess(
         .chunked(11)
         .map { Integer.valueOf(it, 2) }
 
-    val words = indexes.map { list[it] }
-    val extraBits = (flag.length * 8) % 11
-    val newWord = words[if (extraBits == 0) words.size - 1 else words.size - 2]
-    if (phrase.contains(newWord)) {
-//        phrase.remove(newWord)
-        println("  Possible flag: $flag")
-        println("    Words: $words")
-        println("    New word: $newWord")
-        println("    Extra bits: $extraBits")
+    val unusedBits = (flag.length * 8) % 11
+    val flagWords = indexes.map { wordlist[it] }
+    val newWord = flagWords[if (unusedBits == 0) flagWords.size - 1 else flagWords.size - 2]
+    if (words.contains(newWord)) {
+        if (words.size == 2) {
+//            if (flag.startsWith("he2021{f1sh_")) {
 
+            println("  Possible flag: $flag")
+            println("    Words: $flagWords")
+            println("    Unused words: $words")
+//            }
+        }
+//        println("    New word: $newWord")
+//        println("    Extra bits: $unusedBits")
+
+
+        return FlagContext(flag, words.minus(newWord))
     }
+    return null;
 }
+
+class FlagContext(val flag: String, val words: List<String>)
