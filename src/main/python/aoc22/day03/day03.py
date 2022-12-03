3f06eca8947262e73a2d8bec3ca45bc0408096ee
Jan Verner <jan.verner@citrix.com>
2022-12-03 09:34:20 +0100

    Advent of Code 2022

---
+++ src/main/python/aoc22/day03/day03.py
@@ -0,0 +1,46 @@
+from data_input import read_all_lines
+
+
+def letter_priority(letter):
+    if letter.islower():
+        return ord(letter) - ord("`")
+    if letter.isupper():
+        return ord(letter) - ord("&")
+
+
+def star1():
+    total = 0
+    for line in read_all_lines("input.txt"):
+        half = len(line) // 2
+        p1, p2 = set(line[:half]), line[half:]
+
+        for l in p2:
+            if l in p1:
+                total += letter_priority(l)
+                break
+    return total
+
+
+def star2():
+    total = 0
+    counter = 1
+    groups = []
+    for line in read_all_lines("input.txt"):
+        groups.append(set(line))
+        if counter % 3 == 0:
+            g1, g2, g3 = groups
+            for l in g1:
+                if l in g2 and l in g3:
+                    total += letter_priority(l)
+
+            groups = []
+
+        counter += 1
+    return total
+
+
+print(f"Star 1: {star1()}")
+print(f"Star 2: {star2()}")
+
+# Star 1: 7737
+# Star 2: 2697
