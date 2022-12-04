17eee3b875983c1c4a566536e4a8fbf3af768c40
Jan Verner <jan.verner@citrix.com>
2022-12-04 22:07:21 +0100

    Advent of Code 2022

--- src/main/python/aoc22/day04/day04.py
+++ src/main/python/aoc22/day04/day04.py
@@ -1,33 +1,60 @@
+from typing import List
+
 from data_input import read_all_lines
 
-def star1():
+
+def star1(lines: List[str]):
+    """
+    >>> star1(["2-4,6-8",
+    ...        "2-3,4-5",
+    ...        "5-7,7-9",
+    ...        "2-8,3-7",
+    ...        "6-6,4-6",
+    ...        "2-6,4-8",
+    ...        ])
+    2
+    """
+
     total = 0
-    for line in read_all_lines("input.txt"):
+    for line in lines:
         el1, el2 = line.split(",")
         el1_lower, el1_upper = parse_pair(el1)
         el2_lower, el2_upper = parse_pair(el2)
-        if (el1_lower <= el2_lower and el1_upper >= el2_upper) or (el2_lower <= el1_lower and el2_upper >= el1_upper):
+        if ((el1_lower <= el2_lower and el1_upper >= el2_upper) or
+                (el2_lower <= el1_lower and el2_upper >= el1_upper)):
             total += 1
-            print(line)
-        # elif el2_lower <= el1_lower and el2_upper >= el1_upper:
-        #     total += 1
-        #     print(line)
     return total
 
-def star2():
+
+def star2(lines: List[str]):
+    """
+    >>> star2(["2-4,6-8",
+    ...        "2-3,4-5",
+    ...        "5-7,7-9",
+    ...        "2-8,3-7",
+    ...        "6-6,4-6",
+    ...        "2-6,4-8",
+    ...        ])
+    4
+    """
+
     total = 0
-    for line in read_all_lines("input.txt"):
+    for line in lines:
         el1, el2 = line.split(",")
         el1_lower, el1_upper = parse_pair(el1)
         el2_lower, el2_upper = parse_pair(el2)
         if not ((el1_upper < el2_lower) or (el2_upper < el1_lower)):
             total += 1
-            print(line)
     return total
 
-def parse_pair(el1):
+
+def parse_pair(el1: str) -> List[int]:
     return [int(n) for n in el1.split("-")]
 
 
-print(star1())
-print(star2())
+lines = read_all_lines("input.txt")
+print(star1(lines))
+print(star2(lines))
+
+# Star 1: 569
+# Star 2: 936
---
+++ src/main/python/aoc22/day_template/dayXX.py
@@ -0,0 +1,37 @@
+from typing import List
+
+from data_input import read_all_lines
+
+
+def star1(lines: List[str]):
+    """
+    >>> star1([])
+    0
+    """
+
+    answer = 0
+    for line in lines:
+        pass
+
+    return answer
+
+
+def star2(lines: List[str]):
+    """
+    >>> star2([])
+    0
+    """
+
+    answer = 0
+    for line in lines:
+        pass
+
+    return answer
+
+
+lines = read_all_lines("input.txt")
+print(star1(lines))
+print(star2(lines))
+
+# Star 1:
+# Star 2:
