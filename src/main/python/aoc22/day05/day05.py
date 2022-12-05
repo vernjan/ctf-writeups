eee0e96007e790038568df3b7947ce3a8b4f981a
Jan Verner <jverner@purestorage.com>
2022-12-05 14:20:49 +0100

    Advent of Code 2022

--- src/main/python/aoc22/day05/day05.py
+++ src/main/python/aoc22/day05/day05.py
@@ -6,91 +6,54 @@ from data_input import read_all_lines
 
 def star1(lines: List[str]):
     """
-    >>> star1(["    [D]",
-    ...        "[N] [C]",
-    ...        "[Z] [M] [P]",
-    ...        " 1   2   3 ",
-    ...        "",
-    ...        "move 1 from 2 to 1",
-    ...        "move 3 from 1 to 3",
-    ...        "move 2 from 2 to 1",
-    ...        "move 1 from 1 to 2",
-    ...        ])
+    >>> star1(read_all_lines("input-test.txt"))
     'CMZ'
     """
 
-    answer = ""
-    stacks = []
-    for line in lines:
-        # Init stacks
-        if "[" in line:
-            for i in range(1, len(line), 4):
-                letter = line[i]
-                if letter.isupper():
-                    stack_index = i // 4
-                    while stack_index >= len(stacks):
-                        stacks.append([])
-                    stacks[stack_index].insert(0, letter)
-
-        # Do moves
-        if "move" in line:
-            number, _from, to = [int(n) for n in re.findall("[0-9]+", line)]
-            for _ in range(number):
-                pop = stacks[_from - 1].pop()
-                stacks[to - 1].append(pop)
-
-    for stack in stacks:
-        answer += stack[-1]
-
-    return answer
+    return _move_creates(lines, "star1")
 
 
 def star2(lines: List[str]):
     """
-    >>> star2(["    [D]",
-    ...        "[N] [C]",
-    ...        "[Z] [M] [P]",
-    ...        " 1   2   3 ",
-    ...        "",
-    ...        "move 1 from 2 to 1",
-    ...        "move 3 from 1 to 3",
-    ...        "move 2 from 2 to 1",
-    ...        "move 1 from 1 to 2",
-    ...        ])
+    >>> star2(read_all_lines("input-test.txt"))
     'MCD'
     """
 
-    answer = ""
+    return _move_creates(lines, "star2")
+
+
+def _move_creates(lines: List[str], star: str) -> str:
     stacks = []
     for line in lines:
         # Init stacks
         if "[" in line:
             for i in range(1, len(line), 4):
-                letter = line[i]
-                if letter.isupper():
+                if line[i].isupper():
                     stack_index = i // 4
                     while stack_index >= len(stacks):
                         stacks.append([])
-                    stacks[stack_index].insert(0, letter)
+                    stacks[stack_index].insert(0, line[i])
 
         # Do moves
         if "move" in line:
             number, _from, to = [int(n) for n in re.findall("[0-9]+", line)]
-            batch = []
-            for _ in range(number):
-                batch.insert(0, stacks[_from - 1].pop())
-            stacks[to - 1].extend(batch)
+            if star == "star1":
+                for _ in range(number):
+                    pop = stacks[_from - 1].pop()
+                    stacks[to - 1].append(pop)
+            if star == "star2":
+                batch = []
+                for _ in range(number):
+                    pop = stacks[_from - 1].pop()
+                    batch.insert(0, pop)
+                stacks[to - 1].extend(batch)
 
-    for stack in stacks:
-        answer += stack[-1]
-
-    return answer
+    return "".join(stack[-1] for stack in stacks)
 
 
 if __name__ == "__main__":
-    lines = read_all_lines("input.txt")
-    print(star1(lines))
-    print(star2(lines))
+    print(star1(read_all_lines("input.txt")))
+    print(star2(read_all_lines("input.txt")))
 
     # Star 1: QNHWJVJZW
     # Star 2: BPCZJLFJW
---
+++ src/main/python/simple_logging.py
@@ -0,0 +1,4 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(message)s')
+log = logging.getLogger()
