e198143e77f69ab8c75719eadf878a78e7cb7078
Jan Verner <jan.verner@citrix.com>
2022-12-09 09:02:14 +0100

    Advent of Code 2022

--- src/main/python/aoc22/day_09/day09.py
+++ src/main/python/aoc22/day_09/day09.py
@@ -3,13 +3,14 @@ from typing import List
 from data_input import read_all_lines
 from simple_logging import log
 
-# GRID_SIZE = 6
-# START_POS_X = 0
-# START_POS_Y = 5
+GRID_SIZE_ROWS = 36
+GRID_SIZE_COLS = 31
+START_POS_X = 11
+START_POS_Y = 17
 
-GRID_SIZE = 10000
-START_POS_X = 5000
-START_POS_Y = 5000
+# GRID_SIZE = 10000
+# START_POS_X = 5000
+# START_POS_Y = 5000
 
 
 def star1(lines: List[str]):
@@ -58,8 +59,10 @@ def _update_grid(grid, head, head_old, tail, tail_old):
     grid[head[1]][head[0]] = "H"
 
 
+
+
 def _print_grid(grid):
-    # log.info("\n".join(["".join(cell) for cell in grid]))
+    log.info("\n".join(["".join(cell) for cell in grid]))
     pass
 
 
@@ -97,39 +100,49 @@ def star2(lines: List[str]):
     36
     """
 
-
     rope = list([(START_POS_X, START_POS_Y) for _ in range(10)])
 
     visited = set()
     # head = (START_POS_X, START_POS_Y)
     # tail = (START_POS_X, START_POS_Y)
 
-    # Just for debugging
-    # visual_grid = []
-    # for _ in range(GRID_SIZE):
-    #     row = ["." for _ in range(GRID_SIZE)]
-    #     visual_grid.append(row)
-    # _update_grid(visual_grid, head, head, tail, tail)
-    # _print_grid(visual_grid)
+    visual_grid = []
+    for _ in range(GRID_SIZE_ROWS):
+        row = ["." for _ in range(GRID_SIZE_COLS)]
+        visual_grid.append(row)
+    _update_grid_rope(visual_grid, rope)
+    _print_grid(visual_grid)
 
     visited.add((START_POS_X, START_POS_Y))
 
     for line in lines:
         direction, steps = line.split(" ")
-        # log.info(f"{direction} {steps}")
+        log.info(f"{direction} {steps}")
+
 
         for _ in range(int(steps)):
             rope[0] = _move_head(rope[0], direction)
             for i in range(0, len(rope) - 1):
                 rope[i + 1] = _tail_move(rope[i], rope[i + 1])
 
+            _update_grid_rope(visual_grid, rope)
+            _print_grid(visual_grid)
+
             visited.add(rope[-1])
 
-            # _update_grid(visual_grid, head, head_old, tail, tail_old)
 
-        # _print_grid(visual_grid)
+
+
 
     return len(visited)
+
+def _update_grid_rope(grid, rope):
+    for i in range(GRID_SIZE_COLS):
+        for j in range(GRID_SIZE_ROWS):
+            grid[j][i] = "."
+    for i in range(len(rope)):
+        knot = rope[i]
+        grid[knot[1]][knot[0]] = str(i)
 
 
 if __name__ == "__main__":
