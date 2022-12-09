efb54f94d67f359c7393f5ebf6307844862d04c3
Jan Verner <jan.verner@citrix.com>
2022-12-09 07:57:00 +0100

    Advent of Code 2022

--- src/main/python/aoc22/day_09/day09.py
+++ src/main/python/aoc22/day_09/day09.py
@@ -1,16 +1,94 @@
 from typing import List
-from simple_logging import log
 
 from data_input import read_all_lines
+from simple_logging import log
+
+GRID_SIZE = 6
+START_POS_X = 0
+START_POS_Y = 5
+
+GRID_SIZE = 10000
+START_POS_X = 5000
+START_POS_Y = 5000
 
 
 def star1(lines: List[str]):
     """
     >>> star1(read_all_lines("input-test.txt"))
-    'TODO'
+    13
     """
 
+    visited = set()
+    head = (START_POS_X, START_POS_Y)
+    tail = (START_POS_X, START_POS_Y)
+
+    # Just for debugging
+    visual_grid = []
+    for _ in range(GRID_SIZE):
+        row = ["." for _ in range(GRID_SIZE)]
+        visual_grid.append(row)
+    _update_grid(visual_grid, head, head, tail, tail)
+    _print_grid(visual_grid)
+
+    visited.add(tail)
+
+    for line in lines:
+        direction, steps = line.split(" ")
+        # log.info(f"{direction} {steps}")
+
+        for _ in range(int(steps)):
+            head_old = head
+            tail_old = tail
+
+            head = _move_head(head, direction)
+            tail = _tail_move(head, tail)
+            visited.add(tail)
+
+            _update_grid(visual_grid, head, head_old, tail, tail_old)
+
+        _print_grid(visual_grid)
+
+    return len(visited)
+
+
+def _update_grid(grid, head, head_old, tail, tail_old):
+    grid[head_old[1]][head_old[0]] = "."
+    grid[tail_old[1]][tail_old[0]] = "#"
+    grid[tail[1]][tail[0]] = "T"
+    grid[head[1]][head[0]] = "H"
+
+
+def _print_grid(grid):
+    # log.info("\n".join(["".join(cell) for cell in grid]))
     pass
+
+
+def _move_head(head, direction):
+    if direction == "U":
+        return head[0], head[1] - 1
+    if direction == "R":
+        return head[0] + 1, head[1]
+    if direction == "D":
+        return head[0], head[1] + 1
+    if direction == "L":
+        return head[0] - 1, head[1]
+    else:
+        assert False, "Shouldn't happen"
+
+
+def _tail_move(head, tail):
+    # straight moves
+    if head[0] == tail[0] and abs(head[1] - tail[1]) == 2:
+        return head[0], head[1] + (tail[1] - head[1]) // 2
+    if head[1] == tail[1] and abs(head[0] - tail[0]) == 2:
+        return head[0] + (tail[0] - head[0]) // 2, head[1]
+    # diagonal moves
+    if abs(head[0] - tail[0]) > 0 and abs(head[1] - tail[1]) == 2:
+        return head[0], head[1] + (tail[1] - head[1]) // 2
+    if abs(head[1] - tail[1]) > 0 and abs(head[0] - tail[0]) == 2:
+        return head[0] + (tail[0] - head[0]) // 2, head[1]
+
+    return tail
 
 
 def star2(lines: List[str]):
@@ -27,5 +105,5 @@ if __name__ == "__main__":
     print(f"Star 1: {star1(lines)}")
     print(f"Star 2: {star2(lines)}")
 
-    # Star 1:
+    # Star 1: 5874
     # Star 2:
