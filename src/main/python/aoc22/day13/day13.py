2f8bdd3eacdd8241548fa89d4cdfb66e09d08420
Jan Verner <jverner@purestorage.com>
2022-12-13 10:45:16 +0100

    Advent of Code 2022

--- src/main/python/aoc22/day13/day13.py
+++ src/main/python/aoc22/day13/day13.py
@@ -1,8 +1,55 @@
+import functools
 import logging
+from dataclasses import dataclass
 from typing import List
-from simple_logging import log
 
 from data_input import read_all_lines
+from simple_logging import log
+
+
+@functools.total_ordering
+@dataclass
+class SignalList:
+    signals: List
+
+    def __lt__(self, other):
+        return self._compare_lists(self.signals, other.signals) < 0
+
+    @staticmethod
+    def _compare_lists(list1: List, list2: List) -> int:
+        """Returns
+
+          - negative number if list1 < list2
+          - 0 if list1 == list2
+          - positive number if list1 > list2
+        """
+        log.debug(f"{list1} vs. {list2}")
+
+        i = 0
+        while i < len(list1) and i < len(list2):
+            item1 = list1[i]
+            item2 = list2[i]
+            i += 1
+
+            if type(item1) is int and type(item2) is int:
+                if item1 != item2:
+                    return item1 - item2
+            else:
+                if type(item1) is int:
+                    item1 = [item1]
+                elif type(item2) is int:
+                    item2 = [item2]
+                sub_result = SignalList._compare_lists(item1, item2)
+                if sub_result != 0:
+                    return sub_result
+
+        if len(list1) != len(list2):
+            return len(list1) - len(list2)
+
+        return 0
+
+    def __str__(self):
+        return str(self.signals)
 
 
 def star1(lines: List[str]):
@@ -11,29 +58,22 @@ def star1(lines: List[str]):
     13
     """
 
-
-    foo = [[[], 5, [10, 8, 8, [0, 2, 2, 3]], [[2, 0, 8, 9, 6], [10, 3, 4, 5], [3, 6, 1, 2, 2], [7, 3, 5, 7]],
-            [[6, 6, 1], [9], 0]], [6, 8, 10, []], [[0], 9, 9, [[5, 4, 1, 9, 2], [7, 10]]], []]
-
-    log.debug(foo)
-
     index_sum = 0
+    pair_index = 0
 
-    line1 = None
-    line2 = None
+    list1 = None
+    list2 = None
     for i, line in enumerate(lines):
         if i % 3 == 0:
-            line1 = line
+            list1 = SignalList(eval(line))
         elif i % 3 == 1:
-            line2 = line
+            list2 = SignalList(eval(line))
         else:
-
-            brackets1 = _find_matching_brackets(line1)
-            brackets2 = _find_matching_brackets(line2)
-            # log.debug(f"Brackets 1: {brackets1}")
-            # log.debug(f"Brackets 2: {brackets2}")
-            if _signals_equal(line1, 0, -1, brackets1, line2, 0, -1, brackets2):
-                index_sum += i
+            pair_index += 1
+            log.debug(f">>> Comparing {list1} vs. {list2}")
+            if list1 < list2:
+                log.debug(f"Match found: i={i}, pair_index={pair_index}")
+                index_sum += pair_index
 
     return index_sum
 
@@ -41,41 +81,22 @@ def star1(lines: List[str]):
 def star2(lines: List[str]):
     """
     >>> star2(read_all_lines("input-test.txt"))
-    'TODO'
+    140
     """
 
-    pass
+    divider1 = SignalList([[2]])
+    divider2 = SignalList([[6]])
 
+    all_signals = [divider1, divider2]
+    for line in lines:
+        if line:
+            log.debug(f"Creating from line: {line}")
+            all_signals.append(SignalList(eval(line)))
 
-def _signals_equal(line1, from1, to1, brackets1, line2, from2, to2, brackets2):
-    log.debug(f"Comparing {line1[from1, to1]} vs {line2[from2, to2]}")
+    all_signals.sort()
+    log.debug(all_signals)
 
-    while from1 < to1 and from2 < to2:
-        symbol1 = line1[from1]
-        symbol2 = line2[from2]
-
-        if symbol1 == "[" and symbol2 == "[":
-            if _signals_equal(line1, from1 + 1, brackets1[from1] - 1, brackets1,
-                              line2, from2 + 1, brackets2[from2] - 1, brackets2):
-                from1 += brackets1[from1] + 1
-                from2 += brackets2[from2] + 1
-
-        if symbol1 == "[" or symbol2 == "[":
-            pass
-
-    return True
-
-
-def _find_matching_brackets(line):
-    stack = []
-    brackets = {}
-    for i, symbol in enumerate(line):
-        if symbol == "[":
-            stack.append(i)
-        elif symbol == "]":
-            opening_index = stack.pop()
-            brackets[opening_index] = i
-    return brackets
+    return (all_signals.index(divider1) + 1) * (all_signals.index(divider2) + 1)
 
 
 if __name__ == "__main__":
@@ -84,5 +105,5 @@ if __name__ == "__main__":
     print(f"Star 1: {star1(lines)}")
     print(f"Star 2: {star2(lines)}")
 
-    # Star 1:
-    # Star 2:
+    # Star 1: 5557
+    # Star 2: 22425
