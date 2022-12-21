b64396c1f93c186500b4ee1455d7d27c9adb2d82
Jan Verner <jan.verner@citrix.com>
2022-12-21 10:16:59 +0100

    Advent of Code 2022

--- src/main/python/aoc22/day21/day21.py
+++ src/main/python/aoc22/day21/day21.py
@@ -1,52 +1,9 @@
 import logging
-from dataclasses import dataclass
-from typing import List, Any, Dict
+from typing import List
 
 from util.data_io import read_input, read_test_input, timed_run
+from util.ds import FlatNode, TreeNode
 from util.log import log
-
-
-# TODO Move to ds
-
-@dataclass
-class FlatNode:
-    left: str
-    right: str
-    value: Any
-
-
-@dataclass
-class TreeNode:
-    name: str
-    left: "TreeNode" or None
-    right: "TreeNode" or None
-    value: Any
-    depth: int
-
-    @staticmethod
-    def from_nodes(nodes: Dict[str, FlatNode], root_name: str, depth: int = 0) -> "TreeNode" or None:
-        if root_name is None:
-            return None
-
-        node = nodes[root_name]
-        assert node, f"Node {root_name} doesn't exist"
-
-        if node.left is None and node.right is None:
-            return TreeNode(root_name, None, None, node.value, depth)
-
-        left = TreeNode.from_nodes(nodes, node.left, depth + 1)
-        right = TreeNode.from_nodes(nodes, node.right, depth + 1)
-        return TreeNode(root_name, left, right, node.value, depth)
-
-    def __str__(self) -> str:
-        padding = "--" * (self.depth + 1)
-        left = ""
-        if self.left:
-            left = f"\n{padding} {self.left}"
-        right = ""
-        if self.right:
-            right = f"\n{padding} {self.right}"
-        return f"{self.name}: {self.value}{left}{right}"
 
 
 def star1(lines: List[str]):
@@ -60,6 +17,35 @@ def star1(lines: List[str]):
     log.debug(root)
 
     return _eval_node(root)
+
+
+def star2(lines: List[str]):
+    """
+    >>> star2(read_test_input(__file__))
+    301
+    """
+
+    nodes = _parse_flat_nodes(lines)
+    root = TreeNode.from_nodes(nodes, "root")
+    log.debug(root)
+
+    return _eval_human_node(root, None)
+
+
+def _parse_flat_nodes(lines):
+    nodes = {}
+    for node in lines:
+        parts = node.split(" ")
+        name = parts[0][0:-1]
+        left, right = None, None
+        if parts[1].isnumeric():
+            value = int(parts[1])
+        else:
+            left, right = parts[1], parts[3]
+            value = parts[2]
+        nodes[name] = FlatNode(left, right, value)
+    log.debug(nodes)
+    return nodes
 
 
 def _eval_node(node: TreeNode) -> int:
@@ -81,29 +67,42 @@ def _eval_node(node: TreeNode) -> int:
         raise ValueError("What???")
 
 
-def _parse_flat_nodes(lines):
-    nodes = {}
-    for node in lines:
-        parts = node.split(" ")
-        name = parts[0][0:-1]
-        left, right = None, None
-        if parts[1].isnumeric():
-            value = int(parts[1])
+def _eval_human_node(node: TreeNode, node_value: int or None) -> int:
+    if node.name == "humn":
+        log.debug(f"Human value: {node_value}")
+        return node_value
+
+    if isinstance(node.value, int):
+        return node.value
+
+    if node.left.find_node("humn"):
+        human_node = node.left
+        monkeys_value = _eval_node(node.right)
+    else:
+        human_node = node.right
+        monkeys_value = _eval_node(node.left)
+
+    log.debug(f"Human node: {human_node.name}, monkeys value: {monkeys_value}")
+
+    if node.name == "root":
+        return _eval_human_node(human_node, monkeys_value)
+
+    if node.value == "+":
+        return _eval_human_node(human_node, node_value - monkeys_value)
+    elif node.value == "-":
+        if human_node == node.left:
+            return _eval_human_node(human_node, node_value + monkeys_value)
         else:
-            left, right = parts[1], parts[3]
-            value = parts[2]
-        nodes[name] = FlatNode(left, right, value)
-    log.debug(nodes)
-    return nodes
-
-
-def star2(lines: List[str]):
-    """
-    >>> star2(read_test_input(__file__))
-    301
-    """
-
-    pass
+            return _eval_human_node(human_node, monkeys_value - node_value)
+    elif node.value == "*":
+        return _eval_human_node(human_node, node_value // monkeys_value)
+    elif node.value == "/":
+        if human_node == node.left:
+            return _eval_human_node(human_node, node_value * monkeys_value)
+        else:
+            return _eval_human_node(human_node, monkeys_value // node_value)
+    else:
+        raise ValueError("What???")
 
 
 if __name__ == "__main__":
@@ -112,4 +111,4 @@ if __name__ == "__main__":
     timed_run("Star 2", lambda: star2(read_input(__file__)))
 
     # Star 1: 268597611536314
-    # Star 2:
+    # Star 2: 3451534022348
---
+++ src/main/python/aoc22/day22/day22.py
@@ -0,0 +1,32 @@
+import logging
+from typing import List
+
+from util.data_io import read_input, read_test_input, timed_run
+from util.log import log
+
+
+def star1(lines: List[str]):
+    """
+    >>> star1(read_test_input(__file__))
+
+    """
+
+    pass
+
+
+def star2(lines: List[str]):
+    """
+    >>> star2(read_test_input(__file__))
+
+    """
+
+    pass
+
+
+if __name__ == "__main__":
+    log.setLevel(logging.INFO)
+    timed_run("Star 1", lambda: star1(read_input(__file__)))
+    timed_run("Star 2", lambda: star2(read_input(__file__)))
+
+    # Star 1:
+    # Star 2:
