8424f54bdbc2f4b1675b6d725608f8d2ee2943e8
Jan Verner <jverner@purestorage.com>
2022-12-26 00:34:18 +0100

    Advent of Code 2022

--- src/main/python/aoc22/day23/day23.py
+++ src/main/python/aoc22/day23/day23.py
@@ -1,5 +1,6 @@
 import logging
 from typing import List
+import math
 
 from util.data_io import read_input, read_test_input, timed_run
 from util.ds.coord import NORTH, EAST, SOUTH, WEST, NORTH_EAST, SOUTH_EAST, SOUTH_WEST, NORTH_WEST, Direction
@@ -16,17 +17,30 @@ DIRECTIONS_IN_SIGHT = {
 }
 
 
-def star1(lines: List[str], padding_size):
+def star1(lines: List[str], padding_size, max_rounds):
     """
-    >>> star1(read_test_input(__file__), 3)
+    >>> star1(read_test_input(__file__), 3, 10)
     110
     """
 
+    return simulate(lines, max_rounds, padding_size)[1]
+
+
+def star2(lines: List[str], padding_size, max_rounds):
+    """
+    >>> star2(read_test_input(__file__), 3, math.inf)
+    20
+    """
+
+    return simulate(lines, max_rounds, padding_size)[0]
+
+
+def simulate(lines, max_rounds, padding_size):
     grid = Grid(lines, padding_size)
     log.debug(grid)
-
     round_dir = NORTH
-    for _ in range(10):
+    round_number = 1
+    while round_number <= max_rounds:
         proposed_positions = {}
 
         for elf_position in grid.find_all("#"):
@@ -45,35 +59,29 @@ def star1(lines: List[str], padding_size):
                     break
                 elf_dir = _get_next_direction(elf_dir)
 
+        if not proposed_positions:
+            break
+
         accepted_positions = {proposed: elves[0] for proposed, elves in proposed_positions.items() if len(elves) == 1}
         for new_position, old_position in accepted_positions.items():
             grid.set_value(old_position, ".")
             grid.set_value(new_position, "#")
 
-        log.debug(grid)
-        log.debug("---")
+        log.debug(str(grid) + "\n")
+        round_number += 1
         round_dir = _get_next_direction(round_dir)
 
     elves_positions = grid.find_all("#")
-    return _count_empty(elves_positions, grid)
-
-
-def _check_all_empty(grid, positions):
-    return all(map(lambda pos: grid.get_value(pos) == ".", positions))
-
-
-def star2(lines: List[str], padding_size):
-    """
-    >>> star2(read_test_input(__file__))
-    20
-    """
-
-    pass
+    return round_number, _count_empty(elves_positions, grid)
 
 
 def _get_next_direction(start_from: Direction) -> Direction:
     index = DIRECTIONS.index(start_from)
     return DIRECTIONS[(index + 1) % len(DIRECTIONS)]
+
+
+def _check_all_empty(grid, positions):
+    return all(map(lambda pos: grid.get_value(pos) == ".", positions))
 
 
 def _count_empty(elves, grid):
@@ -89,9 +97,9 @@ def _count_empty(elves, grid):
 
 
 if __name__ == "__main__":
-    log.setLevel(logging.DEBUG)
-    timed_run("Star 1", lambda: star1(read_input(__file__), 15))
-    timed_run("Star 2", lambda: star2(read_input(__file__), 10))
+    log.setLevel(logging.INFO)
+    timed_run("Star 1", lambda: star1(read_input(__file__), 10, 10))
+    timed_run("Star 2", lambda: star2(read_input(__file__), 50, math.inf))
 
-    # Star 1:
-    # Star 2:
+    # Star 1: 3987
+    # Star 2: 938
