ed04ad7251caa5ad080428210005275f4025fd87
Jan Verner <jverner@purestorage.com>
2023-11-09 16:29:08 +0100

    AoC23

---
+++ src/main/python/aoc23/day01/day01.py
@@ -0,0 +1,31 @@
+import logging
+
+from util.data_io import read_input, read_test_input, timed_run
+from util.log import log
+
+
+def star1(lines: list[str]):
+    """
+    >>> star1(read_test_input(__file__))
+
+    """
+
+    pass
+
+
+def star2(lines: list[str]):
+    """
+    >>> star2(read_test_input(__file__))
+
+    """
+
+    pass
+
+
+if __name__ == "__main__":
+    log.setLevel(logging.INFO)
+    timed_run("Star 1", lambda: star1(read_input(__file__)))
+    timed_run("Star 2", lambda: star2(read_input(__file__)))
+
+    # Star 1:
+    # Star 2:
