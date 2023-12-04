059ba44c502535ad34a5dec0128c92671693cfb3
Jan Verner <jverner@purestorage.com>
2023-12-04 09:42:32 +0100

    AoC23 - Day 4 Refactor

--- src/main/python/aoc23/day04/day04.py
+++ src/main/python/aoc23/day04/day04.py
@@ -1,5 +1,4 @@
 import logging
-import re
 from collections import defaultdict
 
 from util.data_io import read_input, read_test_input, timed_run
@@ -11,20 +10,7 @@ def star1(lines: list[str]):
     >>> star1(read_test_input(__file__))
     13
     """
-    total = 0
-    for line in lines:
-        game_id = int(re.findall(r"Card\s+(\d+):", line)[0])
-        win, my = line.split(":")[1].split("|")
-        win_numbers = set(win.split())
-        log.debug(f"win_numbers: {win_numbers}")
-        my_numbers = set(my.split())
-        log.debug(f"my_numbers: {my_numbers}")
-        card_value = 0
-        for mn in my_numbers:
-            if mn in win_numbers:
-                card_value = 1 if card_value == 0 else card_value * 2
-        total += card_value
-    return total
+    return solve(lines)[0]
 
 
 def star2(lines: list[str]):
@@ -32,36 +18,30 @@ def star2(lines: list[str]):
     >>> star2(read_test_input(__file__))
     30
     """
-    total_copies = 0
-    card_copies = defaultdict(lambda: 1)
-    for line in lines:
-        game_id = int(re.findall(r"Card\s+(\d+):", line)[0])
-        card_copy_cnt = card_copies[game_id]
-        log.debug(f"game_id: {game_id}, card_copies_cnt: {card_copy_cnt}")
-        win, my = line.split(":")[1].split("|")
-        win_numbers = set(win.split())
-        log.debug(f"win_numbers: {win_numbers}")
-        my_numbers = set(my.split())
-        log.debug(f"my_numbers: {my_numbers}")
-        card_value = 0
-        nc = 0
-        for mn in my_numbers:
-            if mn in win_numbers:
-                nc += 1
-                card_value = 1 if card_value == 0 else card_value * 2
-        log.debug(f"card_value: {card_value}")
-        last = min(nc, len(lines) - game_id)
-        for i in range(last):
-            card_copies[game_id + i + 1] += card_copy_cnt
-        total_copies += card_copy_cnt
-        log.debug(card_copies)
-    return total_copies
+    return solve(lines)[1]
+
+
+def solve(lines: list[str]):
+    total_star1 = 0
+    total_star2 = 0
+    card_copies = defaultdict(lambda: 1)  # star 2
+    for card_id, line in enumerate(lines):
+        card_copies_cnt = card_copies[card_id]
+        win_numbers, my_numbers = [set(numbers.split()) for numbers in line.split(":")[1].split("|")]
+        matching_numbers = len(win_numbers.intersection(my_numbers))
+        card_value = 0 if matching_numbers == 0 else 2 ** (matching_numbers - 1)
+        total_star1 += card_value
+        # star 2: create copies
+        for i in range(min(matching_numbers, len(lines) - card_id - 1)):
+            card_copies[card_id + i + 1] += card_copies_cnt
+        total_star2 += card_copies_cnt
+    return total_star1, total_star2
 
 
 if __name__ == "__main__":
-    log.setLevel(logging.DEBUG)
-    # timed_run("Star 1", lambda: star1(read_input(__file__)))
+    log.setLevel(logging.INFO)
+    timed_run("Star 1", lambda: star1(read_input(__file__)))
     timed_run("Star 2", lambda: star2(read_input(__file__)))
 
-    # Star 1:
-    # Star 2:
+    # Star 1: 21959
+    # Star 2: 5132675
