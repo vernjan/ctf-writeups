1e787cea3763bfbf7984a1616dbf0e7e1b096ed9
Jan Verner <jan.verner@citrix.com>
2023-12-11 22:12:53 +0100

    AoC Day 11

--- src-python/aoc23/day11/day11.py
+++ src-python/aoc23/day11/day11.py
@@ -10,10 +10,22 @@ def star1(lines: list[str]):
     >>> star1(read_test_input(__file__))
     374
     """
+    return _solve(lines, expansion_factor=1)
+
+
+def star2(lines: list[str], expansion_factor):
+    """
+    >>> star2(read_test_input(__file__), expansion_factor=99)
+    8410
+    """
+    return _solve(lines, expansion_factor)
+
+
+def _solve(lines, expansion_factor):
+    width = len(lines)
+
     expanded_rows = []
     expanded_cols = []
-
-    width = len(lines)
     for i in range(width):
         row_has_galaxy = False
         col_has_galaxy = False
@@ -26,9 +38,8 @@ def star1(lines: list[str]):
             expanded_rows.append(i)
         if not col_has_galaxy:
             expanded_cols.append(i)
-
-    log.debug(expanded_rows)
-    log.debug(expanded_cols)
+    log.debug(f"Expanded rows: {expanded_rows}")
+    log.debug(f"Expanded cols: {expanded_cols}")
 
     galaxies = []
     for y in range(width):
@@ -38,16 +49,15 @@ def star1(lines: list[str]):
                 x_expansion = 0
                 for expanded_row in expanded_rows:
                     if y > expanded_row:
-                        y_expansion += 1
+                        y_expansion += expansion_factor
                     else:
                         break
                 for expanded_col in expanded_cols:
                     if x > expanded_col:
-                        x_expansion += 1
+                        x_expansion += expansion_factor
                     else:
                         break
                 galaxies.append(Xy(x + x_expansion, y + y_expansion))
-
     log.debug(galaxies)
 
     total = 0
@@ -55,24 +65,14 @@ def star1(lines: list[str]):
         galaxy = galaxies.pop()
         for other_galaxy in galaxies:
             log.debug("Calculating distance between {} and {}".format(galaxy, other_galaxy))
-            total +=1
-
+            total += abs(galaxy.x - other_galaxy.x) + abs(galaxy.y - other_galaxy.y)
     return total
-
-
-def star2(lines: list[str]):
-    """
-    >>> star2(read_test_input(__file__))
-
-    """
-    for line in lines:
-        pass
 
 
 if __name__ == "__main__":
     log.setLevel(logging.INFO)
     timed_run("Star 1", lambda: star1(read_input(__file__)))
-    timed_run("Star 2", lambda: star2(read_input(__file__)))
+    timed_run("Star 2", lambda: star2(read_input(__file__), expansion_factor=999_999))
 
-    # Star 1:
-    # Star 2:
+    # Star 1: 9769724
+    # Star 2: 603020563700
