b713c949567edc2cfdd6b85c92152c8167cc555a
Jan Verner <jan.verner@citrix.com>
2023-12-12 22:25:17 +0100

    AoC Day 12/s2

--- src-python/aoc23/day12/day12.py
+++ src-python/aoc23/day12/day12.py
@@ -1,6 +1,6 @@
 import logging
 import re
-from typing import List
+from typing import Dict, Tuple
 
 from util.data_io import read_input, read_test_input, timed_run
 from util.log import log
@@ -11,72 +11,80 @@ def star1(lines: list[str]):
     >>> star1(read_test_input(__file__))
     21
     """
-    total = 0
-    for line in lines:
-        pattern, groups = line.split()
-        total_space = len(pattern)
-        groups = list(map(int, groups.split(",")))
-        solutions = _generate_groups(groups, total_space, pattern)
-        for solution in solutions:
-            if re.match(_pattern_to_regex(pattern), solution):
-                log.debug(f"Matching {pattern} against {solution} -> OK")
-                total += 1
-            else:
-                log.debug(f"Matching {pattern} against {solution} -> FAIL")
-
-    return total
+    return _solve(lines)
 
 
 def star2(lines: list[str]):
     """
     >>> star2(read_test_input(__file__))
-
+    525152
     """
+    return _solve(lines, pattern_multiplier=5)
+
+
+def _solve(lines, pattern_multiplier=1):
+    total = 0
+    for line in lines:
+        pattern, groups = line.split()
+        log.debug(f"Pattern: {pattern} / {groups}")
+        pattern = "?".join([pattern] * pattern_multiplier)
+        groups = tuple(map(int, groups.split(","))) * pattern_multiplier
+        total += _count_expanded_groups(groups, pattern, {})
+    return total
+
+
+def _count_expanded_groups(groups: Tuple, pattern: str, cache: Dict) -> int:
+    """
+    >>> _count_expanded_groups((1,), "??", {})
+    2
+    >>> _count_expanded_groups((1,), "#?", {})
+    1
+    >>> _count_expanded_groups((1,), "???", {})
+    3
+    >>> _count_expanded_groups((1, 1), "?????", {})
+    6
+    >>> _count_expanded_groups((1, 1), "#???#", {})
+    1
+    """
+    if not groups:
+        return 1
+    group = groups[0]
+    groups = groups[1:]
+    total_size = len(pattern)
+    group_size = group + 1 if groups else group  # last group doesn't need to end with .
+    tail_size = sum(groups) + max(len(groups) - 1, 0)
+
+    # speed optimization
+    max_start = total_size - group_size - tail_size
+    next_hash = pattern.find("#")
+    if -1 < next_hash < max_start:
+        max_start = min(max_start, next_hash)
+
+    cache_key = (groups, pattern)
+    if cache_key in cache:
+        return cache[cache_key]
+
+    solutions = 0
+    for i in range(max_start + 1):
+        solution = ("_" * i) + ("#" * group) + ("_" * (1 if groups else total_size - i - group))
+        solution_pattern = pattern[:len(solution)]
+        if re.match(_pattern_to_regex(solution_pattern), solution):  # check if the solution is correct
+            tail_pattern = pattern[len(solution):]
+            solutions += _count_expanded_groups(groups, tail_pattern, cache)
+
+    cache[cache_key] = solutions
+
+    return solutions
 
 
 def _pattern_to_regex(pattern: str):
     return pattern.replace(".", "_").replace("?", ".")
 
 
-def _generate_groups(groups: List[int], total_space: int, pattern: str = None):
-    """
-    >>> _generate_groups([1], 2, "??")
-    ['#_', '_#']
-    >>> _generate_groups([1], 2, "#?")
-    ['#_']
-    >>> _generate_groups([1], 3, "???")
-    ['#__', '_#_', '__#']
-    >>> _generate_groups([5], 5, "??#??")
-    ['#####']
-    >>> _generate_groups([1, 1], 3, "??#")
-    ['#_#']
-    >>> _generate_groups([2, 1], 5, "?????")
-    ['##_#_', '##__#', '_##_#']
-    >>> _generate_groups([1, 1], 5, "?????")
-    ['#_#__', '#__#_', '#___#', '_#_#_', '_#__#', '__#_#']
-    >>> _generate_groups([1, 1], 5, "#???#")
-    ['#___#']
-    """
-    if not groups:
-        return [""]
-    group = groups[0]
-    groups = groups[1:]
-    group_size = group + 1 if groups else group  # last group doesn't need to end with .
-    tail_size = sum(groups) + max(len(groups) - 1, 0)
-    max_start = total_space - group_size - tail_size
-    solutions = list()
-    for i in range(max_start + 1):
-        solution = ("_" * i) + ("#" * group) + ("_" * (1 if groups else total_space - i - group))
-        if re.match(_pattern_to_regex(pattern[:len(solution)]), solution):
-            for rec_solution in _generate_groups(groups, total_space - len(solution), pattern[len(solution):]):
-                solutions.append(solution + "".join(rec_solution))
-    return solutions
-
-
 if __name__ == "__main__":
-    log.setLevel(logging.DEBUG)
+    log.setLevel(logging.INFO)
     timed_run("Star 1", lambda: star1(read_input(__file__)))
     timed_run("Star 2", lambda: star2(read_input(__file__)))
 
     # Star 1: 7857
-    # Star 2:
+    # Star 2: 28606137449920
---
+++ src-python/util/misc.py
@@ -0,0 +1,31 @@
+from typing import List
+
+
+def expand_nonogram_groups(groups: List[int], total_size: int) -> List[str]:
+    """
+    >>> expand_nonogram_groups([1], 2)
+    ['#_', '_#']
+    >>> expand_nonogram_groups([1], 3)
+    ['#__', '_#_', '__#']
+    >>> expand_nonogram_groups([5], 6)
+    ['#####_', '_#####']
+    >>> expand_nonogram_groups([1, 1], 3)
+    ['#_#']
+    >>> expand_nonogram_groups([2, 1], 5)
+    ['##_#_', '##__#', '_##_#']
+    >>> expand_nonogram_groups([1, 1], 5)
+    ['#_#__', '#__#_', '#___#', '_#_#_', '_#__#', '__#_#']
+    """
+    if not groups:
+        return [""]
+    group = groups[0]
+    groups = groups[1:]
+    group_size = group + 1 if groups else group  # last group doesn't need to end with .
+    tail_size = sum(groups) + max(len(groups) - 1, 0)
+    max_start = total_size - group_size - tail_size
+    solutions = list()
+    for i in range(max_start + 1):
+        solution = ("_" * i) + ("#" * group) + ("_" * (1 if groups else total_size - i - group))
+        for rec_solution in expand_nonogram_groups(groups, total_size - len(solution)):
+            solutions.append(solution + "".join(rec_solution))
+    return solutions
