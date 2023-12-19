18bf0ba124f72ba1c7a3975955c7b4f2d321548e
Jan Verner <jverner@purestorage.com>
2023-12-19 16:36:36 +0100

    AoC 23 - Day 19 WiP

--- src-python/aoc23/day19/day19.py
+++ src-python/aoc23/day19/day19.py
@@ -1,16 +1,75 @@
+import dataclasses
 import logging
+import re
+from typing import Dict, List, Optional
 
 from util.data_io import read_input, read_test_input, timed_run
 from util.log import log
 
 
+@dataclasses.dataclass(frozen=True)
+class Filter:
+    rating: str
+    operator: str
+    value: int
+
+
+@dataclasses.dataclass(frozen=True)
+class Condition:
+    filter: Optional[Filter]
+    action: str
+
+
+@dataclasses.dataclass(frozen=True)
+class Workflow:
+    id: str
+    conditions: list[Condition] = dataclasses.field(default_factory=list)
+
+
+PART = Dict[str, int]
+
+
 def star1(lines: list[str]):
     """
     >>> star1(read_test_input(__file__))
-
+    19114
     """
+
+    workflows: Dict[str, Workflow] = {}
+    parts: List[PART] = []
+
     for line in lines:
-        pass
+        if line:
+            if line.startswith("{"):
+                parts.append(parse_part(line))
+            else:
+                workflow = parse_workflow(line)
+                workflows[workflow.id] = workflow
+
+    log.debug(workflows)
+    log.debug(parts)
+
+
+def parse_workflow(line) -> Workflow:
+    wid, conditions = re.findall(r"([a-z]+)\{(.*)}", line)[0]
+    workflow = Workflow(wid)
+    for condition in conditions.split(","):
+        if ":" in condition:
+            filter_str, action = condition.split(":")
+            cond_filter = Filter(filter_str[0], filter_str[1], int(filter_str[2:]))
+        else:
+            cond_filter = None
+            action = condition
+        workflow.conditions.append(Condition(cond_filter, action))
+    return workflow
+
+
+def parse_part(line) -> PART:
+    part = {}
+    for rating in line[1:-1].split(","):
+        k, v = rating.split("=")
+        part[k] = int(v)
+    return part
 
 
 def star2(lines: list[str]):
