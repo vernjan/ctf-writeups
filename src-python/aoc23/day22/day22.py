01c5d019750d6d95b038129e223e88977466261b
Jan Verner <jverner@purestorage.com>
2023-12-25 17:58:21 +0100

    AoC 23 - Day 22 S1 WiP

--- src-python/aoc23/day22/day22.py
+++ src-python/aoc23/day22/day22.py
@@ -1,6 +1,6 @@
 import logging
 from collections import defaultdict
-from typing import Dict, List
+from typing import Dict, List, Set
 
 from util.data_io import read_input, read_test_input, timed_run
 from util.ds.coord import Xyz, Cube
@@ -13,16 +13,34 @@ def star1(lines: list[str]):
     5
     """
     cubes: List[Cube] = []
-    cubes_positions: Dict[Xyz, Cube] = {}
     for line in lines:
         points = tuple(map(Xyz.parse, line.split("~")))
         cube = Cube(points[0], points[1])
         cubes.append(cube)
-        for p in cube.positions():
-            cubes_positions[p] = cube
 
     fallen_cubes = _fall(cubes)
-    log.debug(fallen_cubes)
+
+    cubes_position_map: Dict[Xyz, Cube] = {}
+    for fc in fallen_cubes:
+        for p in fc.positions():
+            cubes_position_map[p] = fc
+
+    def all_upper_cubes_has_other_cube_below():
+        for uc in upper_cubes:
+            below_positions: Set[Xyz] = {Xyz(pos.x, pos.y, uc.p1.z - 1) for pos in uc.xy_positions()}
+            below_cubes: Set[Cube] = {cubes_position_map[p] for p in below_positions if p in cubes_position_map}
+            if not below_cubes - {c}:
+                return False
+        return True
+
+    total = 0
+    for c in fallen_cubes:
+        upper_positions: Set[Xyz] = {Xyz(pos.x, pos.y, c.p2.z + 1) for pos in c.xy_positions()}
+        upper_cubes: Set[Cube] = {cubes_position_map[p] for p in upper_positions if p in cubes_position_map}
+        if not upper_cubes or all_upper_cubes_has_other_cube_below():
+            total += 1
+
+    return total
 
 
 def _fall(cubes):
@@ -42,7 +60,6 @@ def _fall(cubes):
         for z in range(fallen_cube.p1.z, fallen_cube.p2.z + 1):
             z_occupied_positions[z] |= fallen_cube.xy_positions()
     log.debug(z_occupied_positions)
-    assert list(z_occupied_positions)[-1] == 6  # TODO remove me, just for test data
     return fallen_cubes
 
 
