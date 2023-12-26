50c36e1c60233e39b4516dad5d72c2fdc67606cc
Jan Verner <jverner@purestorage.com>
2023-12-26 12:29:12 +0100

    AoC 23 - Day 22 S2

--- src-python/aoc23/day22/day22.py
+++ src-python/aoc23/day22/day22.py
@@ -1,6 +1,6 @@
 import logging
 from collections import defaultdict
-from typing import Dict, List, Set
+from typing import List, Set
 
 from util.data_io import read_input, read_test_input, timed_run
 from util.ds.coord import Xyz, Cube
@@ -13,8 +13,7 @@ def star1(lines: list[str]):
     5
     """
     cubes = _fall(_parse_cubes(lines))
-
-    cubes_position_map = get_cubes_position_map(cubes)
+    cubes_position_map = _get_cubes_position_map(cubes)
 
     def all_upper_cubes_has_other_support():
         for uc in upper_cubes:
@@ -32,49 +31,31 @@ def star1(lines: list[str]):
     return total
 
 
-def get_cubes_position_map(cubes):
-    cubes_position_map: Dict[Xyz, Cube] = {}
-    for c in cubes:
-        for p in c.positions:
-            cubes_position_map[p] = c
-    return cubes_position_map
-
-
-def _get_cubes_above(c, cubes_position_map):
-    positions_above: Set[Xyz] = {Xyz(pos.x, pos.y, c.p2.z + 1) for pos in c.xy_positions}
-    return {cubes_position_map[p] for p in positions_above if p in cubes_position_map}
-
-
-def _get_cubes_below(c, cubes_position_map):
-    positions_below: Set[Xyz] = {Xyz(pos.x, pos.y, c.p1.z - 1) for pos in c.xy_positions}
-    return {cubes_position_map[p] for p in positions_below if p in cubes_position_map}
-
-
 def star2(lines: list[str]):
     """
     >>> star2(read_test_input(__file__))
     7
     """
     cubes = _fall(_parse_cubes(lines))
-    cubes_position_map = get_cubes_position_map(cubes)
+    cubes_position_map = _get_cubes_position_map(cubes)
 
     total = 0
-    for bottom_cube in sorted(cubes, key=lambda c: c.p1.z):  # TODO no sorting requird I think
+    for bottom_cube in cubes:
         queue = []
         cpm = cubes_position_map.copy()
         queue.extend(_get_cubes_above(bottom_cube, cpm))
+        for p in bottom_cube.positions:
+            del cpm[p]
         while queue:
-            # queue.sort(key=lambda c: c.p1.z)
             cube = queue.pop(0)
             if cube.p1 not in cpm:
                 continue  # already processed
             cubes_below = _get_cubes_below(cube, cpm)
-            if len(cubes_below) <= 1:
+            if len(cubes_below) == 0:
                 total += 1
                 queue.extend(_get_cubes_above(cube, cpm))
                 for p in cube.positions:
                     del cpm[p]
-
     return total
 
 
@@ -104,6 +85,20 @@ def _fall(cubes):
         for z in range(fallen_cube.p1.z, fallen_cube.p2.z + 1):
             z_occupied_positions[z] |= fallen_cube.xy_positions
     return fallen_cubes
+
+
+def _get_cubes_position_map(cubes):
+    return {p: c for c in cubes for p in c.positions}
+
+
+def _get_cubes_above(c, cubes_position_map):
+    positions_above: Set[Xyz] = {Xyz(pos.x, pos.y, c.p2.z + 1) for pos in c.xy_positions}
+    return {cubes_position_map[p] for p in positions_above if p in cubes_position_map}
+
+
+def _get_cubes_below(c, cubes_position_map):
+    positions_below: Set[Xyz] = {Xyz(pos.x, pos.y, c.p1.z - 1) for pos in c.xy_positions}
+    return {cubes_position_map[p] for p in positions_below if p in cubes_position_map}
 
 
 if __name__ == "__main__":
