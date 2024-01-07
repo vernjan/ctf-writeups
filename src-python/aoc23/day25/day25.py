c84a66b8d2f993f8c651d6ebb20d68b18112148e
Jan Verner <jan.verner@citrix.com>
2024-01-07 23:09:32 +0100

    AoC Day 25 WiP

--- src-python/aoc23/day25/day25.py
+++ src-python/aoc23/day25/day25.py
@@ -1,4 +1,7 @@
 import logging
+import pprint
+from collections import defaultdict
+from typing import Dict, Set
 
 from util.data_io import read_input, read_test_input, timed_run
 from util.log import log
@@ -7,10 +10,16 @@ from util.log import log
 def star1(lines: list[str]):
     """
     >>> star1(read_test_input(__file__))
-
+    54
     """
+    nodes: Dict[str, Set[str]] = defaultdict(set)
     for line in lines:
-        pass
+        node, children = line.split(":")
+        nodes[node].update(children.split())
+        for child in children.split():
+            nodes[child].add(node)
+    log.debug(pprint.pformat(nodes))
+    log.debug(len(nodes))
 
 
 def star2(lines: list[str]):
@@ -23,7 +32,7 @@ def star2(lines: list[str]):
 
 
 if __name__ == "__main__":
-    log.setLevel(logging.INFO)
+    log.setLevel(logging.DEBUG)
     timed_run("Star 1", lambda: star1(read_input(__file__)))
     timed_run("Star 2", lambda: star2(read_input(__file__)))
 
