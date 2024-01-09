c0b4dbdb0cda87b99dc80c2ae9f50d72440545f4
Jan Verner <jverner@purestorage.com>
2024-01-09 13:27:04 +0100

    AoC 23 - Day 25 FAST
    This makes it a bit complicated but much faster

--- src-python/aoc23/day25/day25.py
+++ src-python/aoc23/day25/day25.py
@@ -14,37 +14,71 @@ def star1(lines: list[str]):
     """
     nodes = _parse_nodes(lines)
     node_keys = list(nodes.keys())
-    random.shuffle(node_keys)  # just for fun
+    node_keys_copy = node_keys.copy()
 
-    start_node = node_keys[0]
-    # number of unique paths between all nodes, if there are only 3 unique paths,
-    # then the end_node belongs to the second group
-    path_groups = {3: 0, 4: 1}  # 1 for the start node
-    for end_node in node_keys[1:]:
-        queue = [(start_node, [])]
+    start_node = node_keys_copy.pop()
+    # number of unique paths between all nodes
+    #  key: total unique paths from start node to end node
+    #  value: set of end nodes
+    #  if there are only 3 unique paths, then the end_node must belong to the other group
+    path_groups = {3: set(), 4: {start_node}}
+    path_freq = defaultdict(int)  # optimization - to detect "cut-points", vertices which divide the groups
+    most_frequent_vertices = None  # i.e. cut-points
+    while node_keys_copy:
+        end_node = node_keys_copy.pop()
+        queue = [(start_node, [])]  # node, path
         visited: Set[Tuple[str, str]] = set()
         visited_all_paths: Set[Tuple[str, str]] = set()
-        total_unique_paths = 0
+        total_unique_paths = 0  # between start node and end node
         while queue:
             node, path = queue.pop(0)
             if node == end_node:
                 log.debug(f"{start_node} -> {end_node}: {path}")
                 visited_all_paths |= set(path)  # eliminate already used paths
                 visited = set(visited_all_paths)
-                queue = [(start_node, [])]  # start new search with limited paths
+                # start new search with limited paths
+                # use the last node from the same group, changing start node to quickly detect cut-points
+                queue = [(list(path_groups[4])[-1], [])]
                 total_unique_paths += 1
                 if total_unique_paths > 3:
-                    break
+                    break  # if there are more than 3 unique paths, we're sure the end node belongs to the same group
                 continue
-            for child in nodes[node]:
+            children = list(nodes[node])
+            random.shuffle(children)  # random to detect cut-points
+            for child in children:
                 if (node, child) not in visited:
                     queue.append((child, path + [(node, child)]))
                     visited.add((node, child))
                     visited.add((child, node))
-        path_groups[total_unique_paths] += 1
+        path_groups[total_unique_paths] |= {end_node}
+        if total_unique_paths == 3:  # i.e. the other group (start node and end node belongs to different groups)
+            for vertex in visited_all_paths:
+                path_freq[vertex] += 1
+            most_frequent_vertices = {k: v for k, v in path_freq.items() if v == max(path_freq.values())}
+            if len(most_frequent_vertices) == 3:  # cut-points detected
+                log.debug(f"Bingo, cut-points found: {most_frequent_vertices}")
+                break
 
-    log.debug(path_groups)
-    return path_groups[3] * path_groups[4]
+    # optimization if we have 3 cut-points detected
+    if len(most_frequent_vertices) == 3:
+        # cut into 2 groups
+        nodes_split = nodes.copy()
+        for vertex in most_frequent_vertices:
+            del nodes_split[vertex[0]]
+
+        group1_count = 0
+        queue = [start_node]
+        visited = set()
+        while queue:
+            node = queue.pop(0)
+            if node not in visited:
+                visited.add(node)
+                queue.extend(nodes_split.get(node, []))
+                group1_count += 1
+        group2_count = len(nodes) - group1_count
+        return group1_count * group2_count
+    else:
+        return len(path_groups[3]) * len(path_groups[4])
 
 
 def _parse_nodes(lines):
