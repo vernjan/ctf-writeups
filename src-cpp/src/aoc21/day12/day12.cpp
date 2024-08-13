685459b295774e7717ec2ab1d54950040628da27
Jan Verner <jan.verner@citrix.com>
2024-08-13 22:50:50 +0200

    AoC Day12 S1

--- src-cpp/src/aoc21/day12/day12.cpp
+++ src-cpp/src/aoc21/day12/day12.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 
+#include <algorithm>
 #include <aoc/StarBase.h>
 #include <aoc/aoc_utils.h>
 #include <map>
@@ -12,8 +13,8 @@ struct node {
 };
 
 struct path {
-    vector<string> nodes;
-    set<node> visited;
+    vector<node *> nodes;
+    bool double_visit = false;// TODO Here
 };
 
 void update_node(map<string, node> &nodes, const string &node1_name, const string &node2_name) {
@@ -31,15 +32,33 @@ struct S1 : public StarBase {
     S1() : StarBase(12, 1) {}
 
     [[nodiscard]] size_t execute(const vector<string> &data) const override {
-        map<string, node> nodes;
+        map<string, node> nodes_map;
         for (const auto &line: data) {
             auto link = aoc::split(line, "-");
-            update_node(nodes, link[0], link[1]);
+            update_node(nodes_map, link[0], link[1]);
         }
 
         int total = 0;
-
-
+        node *start_node = &nodes_map["start"];
+        vector<path> paths;
+        paths.emplace_back(vector<node *>{start_node});
+        while (!paths.empty()) {
+            path current_path = paths.back();
+            paths.pop_back();
+            vector<node *> nodes = current_path.nodes;
+            node *current_node = nodes.back();
+            if (current_node->name == "end") {
+                total++;
+            } else {
+                for (node *link: current_node->links) {
+                    if (isupper(link->name[0]) || nodes.end() == find(nodes.begin(), nodes.end(), link)) {
+                        path new_path = current_path;
+                        new_path.nodes.push_back(link);
+                        paths.push_back(new_path);
+                    }
+                }
+            }
+        }
 
         return total;
     }
@@ -56,11 +75,11 @@ struct S2 : public StarBase {
 
 int main() {
     S1 s1;
-    s1.run_test(226);
-    s1.run(0);
+    s1.run_test(10);
+    s1.run(5178);
 
     S2 s2;
-    s2.run_test(0);
+    s2.run_test(36);
     s2.run(0);
 
     return 0;
