b9b3184a3657235f957b92f13cfa98a506d659d4
Jan Verner <jverner@purestorage.com>
2025-12-12 09:42:04 +0100

    WIP

--- src-python/aoc25/day11/day11.py
+++ src-python/aoc25/day11/day11.py
@@ -1,3 +1,4 @@
+import dataclasses
 import logging
 from collections import defaultdict
 
@@ -47,16 +48,67 @@ def star1(lines: list[str]):
     return sum_parent_nodes("out")
 
 
+@dataclasses.dataclass
+class NodeInfo:
+    path_count: int
+    fft = False
+    dac = False
+
 def star2(lines: list[str]):
     """
-    >>> star2(read_test_input(__file__))
+    >>> star2(read_test_input(__file__, input_file="input-test-s2.txt"))
     2
     """
+    graph: dict[str, set[str]] = {}
     for line in lines:
-        pass
+        node, children_str = line.split(": ")
+        children = children_str.split(" ")
+        graph[node] = set(children)
+    log.debug(graph)
+
+    graph_inverted: dict[str, set[str]] = defaultdict(set)
+    for node, children in graph.items():
+        for child in children:
+            graph_inverted[child].add(node)
+    log.debug(graph_inverted)
+
+    # mark visited nodes - node all nodes are on the path
+    visited: set[str] = set()
+    queue = {"svr"}
+    while queue:
+        node = queue.pop()
+        visited.add(node)
+        if node != "out":
+            queue.update(graph[node])
+    log.debug(visited)
+
+    cache: dict[str, NodeInfo] = {}
+
+    def sum_parent_nodes(node: str, fft=False, dac=False) -> int:
+        # if node in cache:
+        #     return cache[node]
+
+        if node not in visited:
+            return 0
+
+        if node == "fft":
+            fft = True
+        elif node == "dac":
+            dac = True
+
+        if node == "svr":
+            return 1 if (fft and dac) else 0
+
+        total = 0
+        for parent in graph_inverted[node]:
+            total += sum_parent_nodes(parent, fft, dac)
+        cache[node] = NodeInfo(total, fft, dac)
+        return total
+
+    return sum_parent_nodes("out")
 
 
 if __name__ == "__main__":
-    log.setLevel(logging.INFO)
-    timed_run("Star 1", lambda: star1(read_input(__file__)), expected_result=None)
+    log.setLevel(logging.DEBUG)
+    timed_run("Star 1", lambda: star1(read_input(__file__)), expected_result=746)
     timed_run("Star 2", lambda: star2(read_input(__file__)), expected_result=None)
