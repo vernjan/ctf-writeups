b2b6590dc9a809e1e8ce6c769d61edc98d665d4c
Jan Verner <jverner@purestorage.com>
2026-01-18 23:02:12 +0100

    AoC 25 Day 12 WIP

--- src-python/aoc25/day12/day12.py
+++ src-python/aoc25/day12/day12.py
@@ -1,6 +1,7 @@
 import logging
+import random
 import re
-import sys
+import string
 from dataclasses import dataclass
 from functools import cached_property
 
@@ -9,57 +10,49 @@ from util.log import log
 
 
 @dataclass(frozen=True)
-class Shape:
+class GiftPattern:
     base: str
 
     @cached_property
-    def variants(self):
-        """
-        All possible flips and rotations
-        """
+    def variants(self) -> set["GiftPattern"]:
         variants = set()
-
-        # TODO JVe Make this generic and move into utils
-
         variants.add(self)
+        # vertical flip
+        variants.add(GiftPattern(
+            self.base[2] + self.base[1] + self.base[0] +
+            self.base[5] + self.base[4] + self.base[3] +
+            self.base[8] + self.base[7] + self.base[6]))
+        # horizontal flip
+        variants.add(GiftPattern(
+            self.base[6] + self.base[7] + self.base[8] +
+            self.base[3] + self.base[4] + self.base[5] +
+            self.base[0] + self.base[1] + self.base[2]))
         # rot 90
-        variants.add(Shape(
+        variants.add(GiftPattern(
             self.base[6] + self.base[3] + self.base[0] +
             self.base[7] + self.base[4] + self.base[1] +
             self.base[8] + self.base[5] + self.base[2]))
         # rot 180
-        variants.add(Shape(
+        variants.add(GiftPattern(
             self.base[8] + self.base[7] + self.base[6] +
             self.base[5] + self.base[4] + self.base[3] +
             self.base[2] + self.base[1] + self.base[0]))
         # rot 270
-        variants.add(Shape(
+        variants.add(GiftPattern(
             self.base[2] + self.base[5] + self.base[8] +
             self.base[1] + self.base[4] + self.base[7] +
             self.base[0] + self.base[3] + self.base[6]))
 
-        # flip
-        variants.add(Shape(
-            self.base[2] + self.base[1] + self.base[0] +
-            self.base[5] + self.base[4] + self.base[3] +
-            self.base[8] + self.base[7] + self.base[6]))
-        # rot 90
-        variants.add(Shape(
-            self.base[0] + self.base[3] + self.base[6] +
-            self.base[1] + self.base[4] + self.base[7] +
-            self.base[2] + self.base[5] + self.base[8]))
-        # rot 180
-        variants.add(Shape(
-            self.base[6] + self.base[7] + self.base[8] +
-            self.base[3] + self.base[4] + self.base[5] +
-            self.base[0] + self.base[1] + self.base[2]))
-        # rot 270
-        variants.add(Shape(
-            self.base[8] + self.base[5] + self.base[2] +
-            self.base[7] + self.base[4] + self.base[1] +
-            self.base[6] + self.base[3] + self.base[0]))
 
         return variants
+
+    def does_match(self, pattern: str) -> bool:
+        for i, p in enumerate(pattern):
+            if p != '.' and self.base[i] == '#':
+                return False
+        return True
+
+
 
     def __str__(self):
         return self.base[:3] + "\n" + self.base[3:6] + "\n" + self.base[6:] + "\n"
@@ -70,9 +63,14 @@ def star1(lines: list[str]):
     >>> star1(read_test_input(__file__))
     2
     """
+    total = 0
+
     gift_patterns: list[str] = []
     gift_pattern: str = ""
     for line in lines:
+        if line and line[0] == '/':
+            continue
+
         if re.match("[#.]{3}", line):
             gift_pattern += line
         elif len(line) == 0:
@@ -81,23 +79,79 @@ def star1(lines: list[str]):
         else:
             m = re.match("([0-9]+)x([0-9]+):(.*)", line)
             if m:
-                # TODO JVe Prepare rotations and flips
-                shapes: list[Shape] = list(map(Shape, gift_patterns))
+                gifts: list[GiftPattern] = list(map(GiftPattern, gift_patterns))
 
-                for s in shapes:
-                    log.debug(f"Shape {s.base}:")
-                    for sv in s.variants:
-                        log.debug(sv)
+                # for gift in gifts:
+                #     log.debug(f"Gift\n{gift.base}:")
+                #     for variant in gift.variants:
+                #         log.debug(variant)
 
-                width = m.group(1)
-                height = m.group(2)
+                width = int(m.group(1))
+                height = int(m.group(2))
                 gifts_histo = list(map(int, m.group(3).split()))
-                _solve_task(width, height, gifts_histo, shapes)
+
+                col = list("." * height)
+                g: list[list[str]] = []
+                for x in range(width):
+                    g.append(col.copy())
+
+                total += _solve_task(width, height, gifts_histo, gifts, g, 0, 0)
+
+    return total
+
+def _solve_task(width: int, height: int, gifts_histo: list[int], gifts: list[GiftPattern], g: list[list[str]], from_x: int, from_y: int):
+    log.info(f"w={width}, h={height}, gh={gifts_histo}, from_x={from_x}, from_y={from_y}")
+
+    # TODO JVe FIX from_x & from_y
+    # TODO JVe Prune suboptimal solutions?
+    #   Based on x I know how many cols I've already covered, then count how many squares were indeed set!
+    #   Fist, I must use depth-first search
+    # TODO JVe Look for squares?
+    for x in range(from_x, width - 2):
+        for y in range(0, height - 2):
+            required_pattern = (g[x][y + 0] + g[x + 1][y + 0] + g[x + 2][y + 0] +
+                                g[x][y + 1] + g[x + 1][y + 1] + g[x + 2][y + 1] +
+                                g[x][y + 2] + g[x + 1][y + 2] + g[x + 2][y + 2])
+            for i, gift_candidate in enumerate(gifts_histo):
+                if gift_candidate == 0:
+                    continue
+                for gift_variant in gifts[i].variants:
+                    if gift_variant.does_match(required_pattern):
+                        log.debug(f"Match found: {gift_variant.base}")
+                        random_l = random.choice(string.ascii_letters)
+                        new_g =[col[:] for col in g]
+                        for j, p in enumerate(gift_variant.base):
+                            if p != ".":
+                                new_g[x + (j % 3)][y + (j // 3)] = random_l
+                        printg(new_g)
+
+                        new_histo = gifts_histo.copy()
+                        new_histo[i] -= 1
+
+                        if sum(new_histo) == 0:
+                            log.info("BINGO - Solved!")
+                            return 1
 
 
-def _solve_task(width: str, height: str, gifts_histo: list[int], shapes: list[Shape]):
-    log.debug(f"w={width}, h={height}, gh={gifts_histo}, shapes={shapes}")
-    sys.exit(0)
+
+                        res = _solve_task(width, height, new_histo, gifts, new_g, x, y)
+                        if res:
+                            return 1
+
+                        # TODO Add into grid
+
+
+
+    return 0
+
+
+def printg(g: list[list[str]]):
+    for y in range(len(g[0])):
+        line = ""
+        for x in range(len(g)):
+            line += g[x][y]
+        log.debug(line)
+
 
 
 def star2(lines: list[str]):
@@ -110,6 +164,6 @@ def star2(lines: list[str]):
 
 
 if __name__ == "__main__":
-    log.setLevel(logging.DEBUG)
+    log.setLevel(logging.INFO)
     timed_run("Star 1", lambda: star1(read_input(__file__)), expected_result=None)
     timed_run("Star 2", lambda: star2(read_input(__file__)), expected_result=None)
